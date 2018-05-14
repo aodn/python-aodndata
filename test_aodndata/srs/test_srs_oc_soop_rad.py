6a0a017f7887fac33900b35d9114dac1b19fefa9
lbesnard <laurent.besnard@utas.edu.au>
2018-05-14 16:40:37 +1000

    SRS OC SOOP RAD - create handler

---
+++ test_aodndata/srs/test_srs_oc_soop_rad.py
@@ -0,0 +1,52 @@
+import os
+import unittest
+
+from aodncore.pipeline.exceptions import InvalidFileNameError
+from aodncore.testlib import HandlerTestCase
+
+from aodndata.srs.srs_oc_soop_rad import SrsOcSoopRadHandler
+
+TEST_ROOT = os.path.join(os.path.dirname(__file__))
+NC_FILE = os.path.join(TEST_ROOT,
+                       'IMOS_SRS-OC_F_20180214T012503Z_VMQ9273_FV01_DALEC_END-20180214T054433Z.nc')
+
+NC_FILE_CREATION_DATE = os.path.join(TEST_ROOT,
+                                     'IMOS_SRS-OC_F_20180214T012503Z_VMQ9273_FV01_DALEC_END-20180214T054433Z_C-20180101T000000Z.nc')
+
+NC_FILE_BAD_1 = os.path.join(TEST_ROOT,
+                       'IMOS_SRS-OC_F_20180214T012503Z_UNKNOWNVESSEL_FV01_DALEC_END-20180214T054433Z.nc')
+
+NC_FILE_BAD_2 = os.path.join(TEST_ROOT,
+                       'IMOS_SRS-OC_F_20180214T012503Z_UNKNOWNVESSEL_FV01_UNKNOWNINSTRUMENT_END-20180214T054433Z.nc')
+
+NC_FILE_BAD_3 = os.path.join(TEST_ROOT,
+                       'BAD.nc')
+
+class TestSrsOcSoopRadHandler(HandlerTestCase):
+    def setUp(self):
+        self.handler_class = SrsOcSoopRadHandler
+        super(TestSrsOcSoopRadHandler, self).setUp()
+
+    def test_good_netcdf(self):
+        dest_path = SrsOcSoopRadHandler.dest_path(NC_FILE)
+        self.assertEqual(dest_path,
+                         os.path.join('IMOS/SRS/OC/radiometer/VMQ9273_Solander/2018', os.path.basename(NC_FILE)))
+
+        # same test as above but checking the creation date is stripped of the end
+        dest_path = SrsOcSoopRadHandler.dest_path(NC_FILE_CREATION_DATE)
+        self.assertEqual(dest_path,
+                         os.path.join('IMOS/SRS/OC/radiometer/VMQ9273_Solander/2018', os.path.basename(NC_FILE)))
+
+    def test_bad_netcdf(self):
+        with self.assertRaises(InvalidFileNameError):
+            SrsOcSoopRadHandler.dest_path(NC_FILE_BAD_1)
+
+        with self.assertRaises(InvalidFileNameError):
+            SrsOcSoopRadHandler.dest_path(NC_FILE_BAD_2)
+
+        with self.assertRaises(InvalidFileNameError):
+            SrsOcSoopRadHandler.dest_path(NC_FILE_BAD_3)
+
+
+if __name__ == '__main__':
+    unittest.main()
