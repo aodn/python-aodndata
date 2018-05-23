72818b49b40e6efe2dbaf9de5aa92248b6dc53f9
bpasquer <benedicte.pasquer@utas.edu.au>
2018-05-23 14:43:30 +1000

    ANFOG : - update dest_path function : remove option for mode - add function to generate path to RT folder based on DM destination path - update unittest: replace original files by file of smaller size for unittest

--- aodndata/anfog/classifiers.py
+++ aodndata/anfog/classifiers.py
@@ -1,8 +1,8 @@
 import os
 import re
 
-from aodncore.pipeline import FileClassifier, PipelineFileCheckType, PipelineFilePublishType, FileType
-from aodncore.pipeline.exceptions import InvalidFileContentError, InvalidInputFileError, InvalidFileNameError
+from aodncore.pipeline import FileClassifier
+from aodncore.pipeline.exceptions import InvalidFileContentError, InvalidFileNameError
 
 
 class AnfogFileClassifier(FileClassifier):
@@ -12,40 +12,16 @@ class AnfogFileClassifier(FileClassifier):
     ANFOG_DM_REGEX = \
         '^IMOS_ANFOG.*_[0-9]{8}T[0-9]{6}Z.*_FV01_timeseries_END-[0-9]{8}T[0-9]{6}Z.nc$'
     DSTG_REGEX = '^DSTO_.*_timeseries_END-[0-9]{8}T[0-9]{6}Z.nc$'
-    DSTG_BASE = 'Department_of_Defence'
+    DSTG_BASE = 'Department_of_Defence/DSTG'
     NRL_REGEX = '^NRL_.*_timeseries_END-[0-9]{8}T[0-9]{6}Z.nc$'
     NRL_BASE = 'US_Naval_Research_Laboratory'
     DM_REGEX = '.*_FV01_timeseries_END-[0-9]{8}T[0-9]{6}Z.nc$'
     FV00_REGEX = '.*_FV00_timeseries_END-[0-9]{8}T[0-9]{6}Z.nc$'
-    RT_PNG_TRANSECT_REGEX = '.*_[0-9]{8}T[0-9]{6}-[0-9]{8}T[0-9]{4}.png$'
+    RT_PNG_TRANSECT_REGEX = '^unit.*_[0-9]{8}T[0-9]{6}-[0-9]{8}T[0-9]{4}.png$'
+    RT_PNG_REGEX = '^unit.*.png$'
     PLATFORM_CODES = {'SG': 'seaglider',
                       'SL': 'slocum_glider'}
-
-    @classmethod
-    def set_data_mode(cls, handler):
-        """Based on ZIP content, define the data mode - DM or RT
-            - DM : netcdf file is FV01
-            - RT : FV00, images (unit???_*.png) and *position_summary.txt
-        """
-        netcdf = handler.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)
-        if not netcdf:
-            raise InvalidInputFileError(
-                "Expecting one NetCDF file in ZIP archive '{zip}'".format(zip=os.path.basename(handler.input_file))
-            )
-
-        for f in netcdf:
-            if re.match(cls.DM_REGEX, f.name):
-                return 'DM'
-            else:
-                # Check that zip content is RT material by looking for .png or position_summary.txt file
-                png = handler.file_collection.filter_by_attribute_value('extension', 'png')
-                position_txtfile = handler.file_collection.filter_by_attribute_regex('name', '.*position_summary.txt$')
-                if (png is not None) or (position_txtfile is not None):
-                    return 'RT'
-                else:
-                    raise InvalidFileContentError(
-                        "Missing ancillary files in ZIP archive {name}".format(
-                            name=os.path.basename(handler.input_file)))
+    RT_POSITION_SUMMARY = '^unit.*position_summary.txt$'
 
     @classmethod
     def get_platform(cls, filename):
@@ -62,65 +38,63 @@ class AnfogFileClassifier(FileClassifier):
         return platform
 
     @classmethod
-    def get_deployment_code(cls, src_file):
+    def get_deployment_code(cls, src_path):
         """Depending on data mode :
            DM :  get deployment code from netcdf global attributes directly
                DSTG : no attribute deployment_code, extract deployment code from title instead
            RT :exctract deployment code from title
         """
-        if re.match(cls.DSTG_REGEX, src_file.name) or re.match(cls.ANFOG_RT_REGEX, src_file.name):
-            title = cls._get_nc_att(src_file.src_path, 'title')
+        name = os.path.basename(src_path)
+        if re.match(cls.DSTG_REGEX, name) or re.match(cls.ANFOG_RT_REGEX, name):
+            title = cls._get_nc_att(src_path, 'title')
             deployment_code = title.split()[-1]
             if deployment_code == 'mission':
                 raise InvalidFileContentError(
-                    "Missing deployment code in {file} ".format(file=os.path.basename(src_file.name)))
+                    "Missing deployment code in {file} ".format(file=name))
 
-        elif re.match(cls.ANFOG_DM_REGEX, src_file.name):
-            deployment_code = cls._get_nc_att(src_file.src_path, 'deployment_code')
+        elif re.match(cls.ANFOG_DM_REGEX, name):
+            deployment_code = cls._get_nc_att(src_path, 'deployment_code')
         else:
             raise InvalidFileNameError(
-                "Invalidfile name {file} ".format(file=os.path.basename(src_file.name)))
+                "Invalidfile name {file} ".format(file=name))
 
         return deployment_code
 
     @classmethod
-    def set_attributes_per_file_type(cls, filecollection, destination, path):
-        """
-        Set check_type,publish_type and dest_path or archive_path attribute
-           to a collection of file of the same type/format according to its destination
-           destination is either 'S3upload' or 'archive'
-        """
-        for f in filecollection:
-            f.check_type = PipelineFileCheckType.FORMAT_CHECK
-            if destination == 'S3upload':
-                f.publish_type = PipelineFilePublishType.UPLOAD_ONLY
-                f.dest_path = os.path.join(path, f.name)
-            elif destination == 'archive':
-                f.publish_type = PipelineFilePublishType.ARCHIVE_ONLY
-                f.archive_path = os.path.join(path, f.name)
+    def make_rt_path(cls, path):
+        """Convert a Delayed mode path like:
+        IMOS/ANFOG/platform/deployment_code/
+        in a REALTIME path like :
+        IMOS/ANFOG/REALTIME/platform/deployment_code/ """
+        hierarchy = path.split('/')
+        assert len(hierarchy) == 4, "Path should have 4 parts "
+        rt_path = []
+        rt_path.extend([hierarchy[0], hierarchy[1]])
+        rt_path.append('REALTIME')
+        rt_path.extend([hierarchy[2], hierarchy[3]])
+
+        return cls._make_path(rt_path)
 
     @classmethod
-    def dest_path(cls, src_file, data_mode):
+    def dest_path(cls, src_file):
         dir_list = []
         fields = cls._get_file_name_fields(src_file.name)
         platform = cls.get_platform(src_file.name)
-        deployment_code = cls.get_deployment_code(src_file)
+        deployment_code = cls.get_deployment_code(src_file.src_path)
 
-        if fields[0] == 'NRL':
+        if re.match(cls.NRL_REGEX, src_file.name):
             project = cls.NRL_BASE
-            dir_list.extend([project])
-        elif fields[0] == 'DSTO':
+            dir_list.append(project)
+        elif re.match(cls.DSTG_REGEX, src_file.name):
             project = cls.DSTG_BASE
-            facility = 'DSTG'
-            dir_list.extend([project, facility])
-        else:  # IMOS ANFOG
+            dir_list.append(project)
+        else:  # IMOS ANFOG RT or DM
             project = fields[0]
             facility = fields[1]
             dir_list.extend([project, facility])
-
-        if data_mode == 'RT':
-            data_type = 'REALTIME'
-            dir_list.append(data_type)
+            if re.match(cls.ANFOG_RT_REGEX, src_file.name):
+                data_type = 'REALTIME'
+                dir_list.append(data_type)
 
         dir_list.extend([platform, deployment_code])
 
