ac5258eaf7c2d819e025928ec8e08f9d9ce6e86b
bpasquer <benedicte.pasquer@utas.edu.au>
2018-05-30 16:59:36 +1000

    Unittest for deletion of file on S3  - add  _test_gps.csv and netcdf file with different new creation date - improve main handler function to handle previous versions.

--- test_aodndata/soop/test_soop_ba.py
+++ test_aodndata/soop/test_soop_ba.py
@@ -1,21 +1,25 @@
 import os
 import unittest
 
-from aodncore.pipeline import PipelineFilePublishType, FileType
+from aodncore.pipeline import PipelineFilePublishType, FileType, PipelineFile, PipelineFileCollection
 from aodncore.testlib import HandlerTestCase
 from aodncore.pipeline.exceptions import InvalidInputFileError, InvalidFileContentError
 from aodndata.soop.soop_ba import SoopBaHandler
+from aodncore.pipeline.storage import get_storage_broker
 
 TEST_ROOT = os.path.join(os.path.dirname(__file__))
+PREV_NC = os.path.join(TEST_ROOT,
+                       'IMOS_SOOP-BA_A_20160116T152139Z_VKAD_FV02_Antarctic-Discovery'
+                       '-ES60-38_END-20160129T062056Z_C-20160101T000000Z.nc')
 GOOD_NC = os.path.join(TEST_ROOT,
                        'IMOS_SOOP-BA_A_20160116T152139Z_VKAD_FV02_Antarctic-Discovery'
                        '-ES60-38_END-20160129T062056Z_C-20170227T055245Z.nc')
-
 BAD_NC = os.path.join(TEST_ROOT,
                       'IMOS_SOOP-BA_A_20160116T152139Z_VKAD_FV02_Antarctic-Discovery'
                       '-ES60-38_END-20160129T062056Z_C-20170227T055245Z_BAD.nc')
 GOOD_ZIP = os.path.join(TEST_ROOT, 'Antarctic-Discovery_20160116-20160129.zip')
 BAD_ZIP = os.path.join(TEST_ROOT, 'Antarctic-Discovery_20160116-20160129_BAD.zip')
+CSV = os.path.join(TEST_ROOT, 'Antarctic_Discovery_20160115-20160129_test.gps.csv')
 
 
 class TestSoopBaHandler(HandlerTestCase):
@@ -48,6 +52,7 @@ class TestSoopBaHandler(HandlerTestCase):
     def test_good_nc_zip(self):
         handler = self.run_handler(GOOD_ZIP)
         nc_files = handler.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)
+
         f = nc_files[0]
         self.assertEqual(f.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
         self.assertEqual(f.name, os.path.basename(GOOD_NC))
@@ -76,6 +81,53 @@ class TestSoopBaHandler(HandlerTestCase):
         """Test with invalid netcdf missing a report_id"""
         self.run_handler_with_exception(InvalidFileContentError, BAD_NC)
 
+    def test_setup_upload_location(self):
+        # create some PipelineFiles to represent the existing files on 'S3'
+        preexisting_files = PipelineFileCollection()
 
-if __name__ == '__main__':
-    unittest.main()
+        existing_file1 = PipelineFile(PREV_NC, dest_path=os.path.join(
+            'IMOS/SOOP/SOOP-BA/VKAD_Antarctic-Discovery/Antarctic-Discovery_20160116-20160129/',
+            os.path.basename(PREV_NC)))
+
+        existing_file2 = PipelineFile(CSV, dest_path=os.path.join(
+            'IMOS/SOOP/SOOP-BA/VKAD_Antarctic-Discovery/Antarctic-Discovery_20160116-20160129/',
+            os.path.basename(CSV)))
+        preexisting_files.update([existing_file1, existing_file2])
+
+        # set the files to UPLOAD_ONLY
+        preexisting_files.set_default_publish_types(include_regexes='.*', exclude_regexes=None,
+                                                    addition_type=PipelineFilePublishType.UPLOAD_ONLY,
+                                                    deletion_type=PipelineFilePublishType.NO_ACTION)
+
+        # upload the 'preexisting_files' collection to the unit test's temporary upload location
+        broker = get_storage_broker(self.config.pipeline_config['global']['upload_uri'])
+        broker.upload(preexisting_files)
+
+        # run the handler
+        handler = self.run_handler(GOOD_ZIP)
+
+        # add some tests to make sure the previous files were handled appropriately, e.g.
+        # - they were added as deletions
+        # - they were successfully deleted
+        # - they were the *only* ones deleted
+        nc_files = handler.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)
+
+        for nc in nc_files:
+            if nc.name == os.path.basename(PREV_NC):
+                self.assertEqual(nc.publish_type, PipelineFilePublishType.DELETE_UNHARVEST)
+                self.assertEqual(nc.is_deleted, True)
+            else:
+                self.assertEqual(nc.is_deleted, False)
+        csvs = handler.file_collection.filter_by_attribute_id('file_type', FileType.CSV)
+        for csv in csvs:
+            if csv.name == os.path.basename(CSV):
+                self.assertEqual(csv.publish_type, PipelineFilePublishType.DELETE_ONLY)
+                self.assertEqual(csv.is_deleted, True)
+            else:
+                self.assertEqual(csv.is_deleted, False)
+
+        png = handler.file_collection.filter_by_attribute_id('file_type', FileType.PNG)
+        self.assertEqual(png[0].is_deleted, False)
+
+        if __name__ == '__main__':
+            unittest.main()
