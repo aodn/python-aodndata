af9e5e326536bfd0361128f90bd55e444dd9c9e4
lbesnard <laurent.besnard@utas.edu.au>
2018-12-17 12:32:36 +1100

    SRS SURFACE WAVES - destpath create dir per bin

--- aodndata/srs/srs_surface_waves.py
+++ aodndata/srs/srs_surface_waves.py
@@ -26,7 +26,8 @@ def dest_path_srs_surface_waves(filepath):
     FILE_PATTERN = re.compile(r"""
                                 IMOS_SRS-Surface-Waves_MW_
                                 (?P<platform_code>{})_FV02_
-                                (?P<latitude>[0-9]{{3}})(S|N)-
+                                (?P<latitude>[0-9]{{3}})
+                                (?P<latitude_dir>(S|N))-
                                 (?P<longitude>[0-9]{{3}})E-
                                 DM00\.nc$
                                 """.format(platforms), re.VERBOSE)
@@ -34,11 +35,36 @@ def dest_path_srs_surface_waves(filepath):
     file_basename = os.path.basename(filepath)
     if FILE_PATTERN.match(file_basename):
         fields = get_pattern_subgroups_from_string(file_basename, FILE_PATTERN)
+        lat = int(fields['latitude'])
+        lon = int(fields['longitude'])
+        lat_dir = fields['latitude_dir']
+
+        def coord_lower_limit_val(val):
+            """ return the bin bottom left coordinate of a coordinate value
+            exampe: 26 -> 20
+                    31 -> 30
+                    0 -> 0
+                    10 -> 10
+                    """
+            return int(round(val / 10) * 10)
+
+        if lat_dir == 'S':
+            lat_bottom_left = coord_lower_limit_val(lat) + 10
+        elif lat_dir == 'N':
+            lat_bottom_left = coord_lower_limit_val(lat)
+
+        lon_bottom_left = coord_lower_limit_val(lon)
+
+        coord_dirname = '{lat_bottom_left}{lat_dir}_{lon_bottom_left}E'.format(
+            lat_bottom_left=str(lat_bottom_left).zfill(3),
+            lon_bottom_left=str(lon_bottom_left).zfill(3),
+            lat_dir=lat_dir)
+
     else:
         raise InvalidFileNameError(
             "file name: \"{filename}\" not matching regex to deduce dest_path".format(
                 filename=os.path.basename(filepath)))
 
-    return os.path.join(PREFIX_PATH, fields['platform_code'], file_basename)
+    return os.path.join(PREFIX_PATH, fields['platform_code'], coord_dirname, file_basename)
 
 
--- test_aodndata/srs/test_srs_surface_waves.py
+++ test_aodndata/srs/test_srs_surface_waves.py
@@ -12,13 +12,21 @@ class TestSrsSurfaceWavesHandler(unittest.TestCase):
 
     def test_dest_path_srs_surface_waves(self):
         good_nc_north = os.path.join(TEST_ROOT, 'IMOS_SRS-Surface-Waves_MW_CRYOSAT-2_FV02_052N-000E-DM00.nc')
-        self.assertEqual(dest_path_srs_surface_waves(good_nc_north), 'IMOS/SRS/Surface-Waves/Wave-Wind-Altimetry-DM00/CRYOSAT-2/{basename}'.format(
+        self.assertEqual(dest_path_srs_surface_waves(good_nc_north),
+                         'IMOS/SRS/Surface-Waves/Wave-Wind-Altimetry-DM00/CRYOSAT-2/050N_000E/{basename}'.format(
             basename=os.path.basename(good_nc_north)
         ))
 
         good_nc_south = os.path.join(TEST_ROOT, 'IMOS_SRS-Surface-Waves_MW_CRYOSAT-2_FV02_052S-000E-DM00.nc')
-        self.assertEqual(dest_path_srs_surface_waves(good_nc_south), 'IMOS/SRS/Surface-Waves/Wave-Wind-Altimetry-DM00/CRYOSAT-2/{basename}'.format(
+        self.assertEqual(dest_path_srs_surface_waves(good_nc_south),
+                         'IMOS/SRS/Surface-Waves/Wave-Wind-Altimetry-DM00/CRYOSAT-2/060S_000E/{basename}'.format(
             basename=os.path.basename(good_nc_south)
+        ))
+
+        good_nc_0_south = os.path.join(TEST_ROOT, 'IMOS_SRS-Surface-Waves_MW_CRYOSAT-2_FV02_002S-013E-DM00.nc')
+        self.assertEqual(dest_path_srs_surface_waves(good_nc_0_south),
+                         'IMOS/SRS/Surface-Waves/Wave-Wind-Altimetry-DM00/CRYOSAT-2/010S_010E/{basename}'.format(
+            basename=os.path.basename(good_nc_0_south)
         ))
 
         bad_product = os.path.join(TEST_ROOT, 'IMOS_SRS-Surface-Waves_MW_UNKNOWN-2_FV02_052S-000E-DM00.nc')
