0dc00376c5f8830156334786f690fef3388cda22
lbesnard <laurent.besnard@utas.edu.au>
2019-01-14 15:42:36 +1100

    SRS GHRSST - add path creation for VIIRS and multisensor data

--- test_aodndata/srs/test_srs_ghrsst.py
+++ test_aodndata/srs/test_srs_ghrsst.py
@@ -61,9 +61,32 @@ SRS_VARIOUS = {'19950309232523-ABOM-L3U_GHRSST-SSTskin-AVHRR09_D-Des_Southern.nc
                '20151201185911-ABOM-L3U_GHRSST-SSTskin-AVHRR19_D-Des.nc': 'SRS/SST/ghrsst/L3U/n19/2015',
                '20140428-ABOM-L3P_GHRSST-SSTsubskin-AVHRR_MOSAIC_01km-AO_DAAC.nc': 'SRS/SST/ghrsst/L3P/14d/2014',
                '20180612120000-ABOM-L4_GHRSST-SSTfnd-RAMSSA_09km-AUS-v02.0-fv01.0.nc': 'SRS/SST/ghrsst/L4/RAMSSA/2018',
-               '20180613120000-ABOM-L4_GHRSST-SSTfnd-GAMSSA_28km-GLOB-v02.0-fv01.0.nc': 'SRS/SST/ghrsst/L4/GAMSSA/2018'
-
+               '20180613120000-ABOM-L4_GHRSST-SSTfnd-GAMSSA_28km-GLOB-v02.0-fv01.0.nc': 'SRS/SST/ghrsst/L4/GAMSSA/2018',
+               '20190109152000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-1d_night.nc': 'SRS/SST/ghrsst/L3SM-1d/ngt/2019',
+               '20190109152000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-1d_night_Southern.nc': 'SRS/SST/ghrsst/L3SM-1dS/ngt/2019',
+               '20180101032000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-1d_day.nc': 'SRS/SST/ghrsst/L3SM-1d/day/2018',
+               '20180101092000-ABOM-L3S_GHRSST-SSTfnd-MultiSensor-1d_dn.nc': 'SRS/SST/ghrsst/L3SM-1d/dn/2018',
+               '20180101152000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-1d_night.nc': 'SRS/SST/ghrsst/L3SM-1d/ngt/2018',
+               '20180131032000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-1m_day.nc': 'SRS/SST/ghrsst/L3SM-1m/day/2018',
+               '20180131092000-ABOM-L3S_GHRSST-SSTfnd-MultiSensor-1m_dn.nc': 'SRS/SST/ghrsst/L3SM-1m/dn/2018',
+               '20180131152000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-1m_night.nc': 'SRS/SST/ghrsst/L3SM-1m/ngt/2018',
+               '20180131111000-ABOM-L3S_GHRSST-SSTfnd-MultiSensor-1m_dn_Southern.nc': 'SRS/SST/ghrsst/L3SM-1mS/dn/2018',
+               '20180101152000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-6d_day.nc': 'SRS/SST/ghrsst/L3SM-6d/day/2018',
+               '20180101212000-ABOM-L3S_GHRSST-SSTfnd-MultiSensor-6d_dn.nc': 'SRS/SST/ghrsst/L3SM-6d/dn/2018',
+               '20180101032000-ABOM-L3S_GHRSST-SSTskin-MultiSensor-6d_night.nc': 'SRS/SST/ghrsst/L3SM-6d/ngt/2018',
+               '20180101010000-ABOM-L3U_GHRSST-SSTskin-VIIRS_NPP-Pol_Southern.nc': 'SRS/SST/ghrsst/L3U-S/snpp/2018',
+               '20180101011000-ABOM-L3U_GHRSST-SSTskin-VIIRS_NPP-Pol.nc': 'SRS/SST/ghrsst/L3U/snpp/2018',
+               '20180101032000-ABOM-L3C_GHRSST-SSTskin-VIIRS_NPP-1d_day.nc': 'SRS/SST/ghrsst/L3C-1d/day/snpp/2018',
+               '20180101152000-ABOM-L3C_GHRSST-SSTskin-VIIRS_NPP-1d_night.nc': 'SRS/SST/ghrsst/L3C-1d/ngt/snpp/2018',
+               '20180101051000-ABOM-L3C_GHRSST-SSTskin-VIIRS_NPP-1d_day_Southern.nc': 'SRS/SST/ghrsst/L3C-1dS/day/snpp/2018',
+               '20180101171000-ABOM-L3C_GHRSST-SSTskin-VIIRS_NPP-1d_night_Southern.nc': 'SRS/SST/ghrsst/L3C-1dS/ngt/snpp/2018',
+               '20180101111000-ABOM-L3S_GHRSST-SSTfnd-MultiSensor-1d_dn_Southern.nc': 'SRS/SST/ghrsst/L3SM-1dS/dn/2018',
+               '20180131111000-ABOM-L3S_GHRSST-SSTfnd-MultiSensor-1m_dn_Southern.nc': 'SRS/SST/ghrsst/L3SM-1mS/dn/2018',
+               '20180101010000-ABOM-L3U_GHRSST-SSTskin-VIIRS_NPP-Pol_Southern.nc': 'SRS/SST/ghrsst/L3U-S/snpp/2018',
+               '20180101051000-ABOM-L3C_GHRSST-SSTskin-VIIRS_NPP-1d_day_Southern.nc': 'SRS/SST/ghrsst/L3C-1dS/day/snpp/2018',
+               '20180101171000-ABOM-L3C_GHRSST-SSTskin-VIIRS_NPP-1d_night_Southern.nc': 'SRS/SST/ghrsst/L3C-1dS/ngt/snpp/2018'
                }
+                              
 
 SRS_BAD = {'19950309232523-ABOM-UNKNOWN_GHRSST-SSTskin-AVHRR09_D-Des_Southern.nc',
            '19950309232523-ABOM-L3U_GHRSST-SSTskin-AVHRR09_D-Des_South.nc',
@@ -86,49 +109,43 @@ class TestSrsGhrsstHandler(HandlerTestCase):
         f = handler.file_collection[0]
         self.assertEqual(f.check_type, PipelineFileCheckType.NC_COMPLIANCE_CHECK)
         self.assertEqual(f.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
-        self.assertEqual(f.dest_path,
-                         os.path.join('IMOS/SRS/SST/ghrsst/L3S-1d/day/1992', os.path.basename(SRS_L3S_1D_DAY)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L3S-1d/day/1992', os.path.basename(SRS_L3S_1D_DAY)), f.dest_path)
         self.assertTrue(f.is_checked)
         self.assertTrue(f.is_stored)
 
     def test_l3s_1ds_path(self):
         dest_path = SrsGhrsstHandler.dest_path(SRS_L3S_1DS_DAY)
-        self.assertEqual(dest_path,
-                         os.path.join('IMOS/SRS/SST/ghrsst/L3S-1dS/dn/2017', os.path.basename(SRS_L3S_1DS_DAY)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L3S-1dS/dn/2017', os.path.basename(SRS_L3S_1DS_DAY)), dest_path)
 
     def test_l3c_1d_path(self):
         # test with sat number in path
         dest_path = SrsGhrsstHandler.dest_path(SRS_L3C_1D_DAY)
-        self.assertEqual(dest_path,
-                         os.path.join('IMOS/SRS/SST/ghrsst/L3C-1d/day/n19/2017', os.path.basename(SRS_L3C_1D_DAY)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L3C-1d/day/n19/2017', os.path.basename(SRS_L3C_1D_DAY)), dest_path)
 
     def test_l3c_1ds_path(self):
         # test with sat number in path
         dest_path = SrsGhrsstHandler.dest_path(SRS_L3C_1DS_NGT)
-        self.assertEqual(dest_path,
-                         os.path.join('IMOS/SRS/SST/ghrsst/L3C-1dS/ngt/n19/2017', os.path.basename(SRS_L3C_1DS_NGT)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L3C-1dS/ngt/n19/2017', os.path.basename(SRS_L3C_1DS_NGT)), dest_path)
 
     def test_l3u_path(self):
         dest_path = SrsGhrsstHandler.dest_path(SRS_L3U)
-        self.assertEqual(dest_path, os.path.join('IMOS/SRS/SST/ghrsst/L3U/n11/1994', os.path.basename(SRS_L3U)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L3U/n11/1994', os.path.basename(SRS_L3U)), dest_path)
 
     def test_l3p_path(self):
         dest_path = SrsGhrsstHandler.dest_path(SRS_L3P)
-        self.assertEqual(dest_path, os.path.join('IMOS/SRS/SST/ghrsst/L3P/14d/2014/', os.path.basename(SRS_L3P)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L3P/14d/2014/', os.path.basename(SRS_L3P)), dest_path)
 
     def test_l4_path(self):
         dest_path = SrsGhrsstHandler.dest_path(SRS_L4_RAMSSA)
-        self.assertEqual(dest_path,
-                         os.path.join('IMOS/SRS/SST/ghrsst/L4/RAMSSA/2018/', os.path.basename(SRS_L4_RAMSSA)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L4/RAMSSA/2018/', os.path.basename(SRS_L4_RAMSSA)), dest_path)
 
         dest_path = SrsGhrsstHandler.dest_path(SRS_L4_GAMSSA)
-        self.assertEqual(dest_path,
-                         os.path.join('IMOS/SRS/SST/ghrsst/L4/GAMSSA/2018/', os.path.basename(SRS_L4_GAMSSA)))
+        self.assertEqual(os.path.join('IMOS/SRS/SST/ghrsst/L4/GAMSSA/2018/', os.path.basename(SRS_L4_GAMSSA)), dest_path)
 
     def test_various_path(self):
         for nc_file in SRS_VARIOUS.keys():
             dest_path = SrsGhrsstHandler.dest_path(os.path.join(TEST_ROOT, nc_file))
-            self.assertEqual(dest_path, os.path.join('IMOS', SRS_VARIOUS[nc_file], nc_file))
+            self.assertEqual(os.path.join('IMOS', SRS_VARIOUS[nc_file], nc_file), dest_path)
 
     def test_various_bad_path(self):
         for nc_file in SRS_BAD:
