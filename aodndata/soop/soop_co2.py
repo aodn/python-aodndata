293eeafa2ad789ffb12e9851ff61572ff9a6b2bf
lbesnard <laurent.besnard@utas.edu.au>
2019-03-20 16:58:10 +1100

    SOOP CO2 - update code to fix unitest. use ncwriter

--- aodndata/soop/soop_co2.py
+++ aodndata/soop/soop_co2.py
@@ -28,111 +28,17 @@ def def_project(src_file):
     eg: 'IMOS/SOOP/SOOP-CO2/VNAA_Aurora-Australis/2017/AA1617_V3/'
     """
 
-    if src_file.name.endswith('.nc'):
-        fields = FileClassifier._get_file_name_fields(src_file.name, min_fields=5)
-        if 'FV00' in src_file.name:
+    if src_file.endswith('.nc'):
+        fields = FileClassifier._get_file_name_fields(src_file, min_fields=5)
+        if 'FV00' in src_file:
             return fields[1] + '_RT'  # <Facility-Name + RTnc suffix>
         else:
             return fields[0]  # <Project-Name>
     else:
         raise InvalidFileFormatError(
             "'{name}'is not a valid RT CO2 data (*dat.txt) or not a NetCDF file.".format(
-                name=src_file.name)
+                name=src_file)
         )
-
-
-def dest_path_soop_co2(src_file):
-
-    dir_list = []
-    project = def_project(src_file)
-    ship_callsign_ls = ship_callsign_list()
-
-    if project not in VALID_PROJECT:
-        raise InvalidFileNameError(
-            "Invalid project name '{project}'. "
-            "Project should be IMOS, SOOP-CO2_RT or Future_Reef_MAP".format(project=project))
-
-    if project in ['IMOS', 'SOOP-CO2_RT']:
-        fields = FileClassifier._get_file_name_fields(src_file.name)
-        ship_code = fields[4]
-        if ship_code not in ship_callsign_ls:
-            raise InvalidFileNameError(
-                "Missing vessel callsign in file name '{name}'.".format(name=src_file.name))
-
-        project_base = 'IMOS'
-        facility = fields[1][:4]
-        sub_facility = fields[1]
-        platform = "{ship_code}_{ship_name}".format(ship_code=ship_code, ship_name=ship_callsign_ls[ship_code])
-        dir_list.extend([project_base, facility, sub_facility, platform])
-
-    if project == 'FutureReefMap':
-        fields = FileClassifier._get_file_name_fields(src_file.name, min_fields=5)
-        ship_code = fields[3]
-        if ship_code not in ship_callsign_ls:
-            raise InvalidFileNameError(
-                "Missing vessel callsign in file name '{name}'.".format(name=src_file.name))
-
-        dir_list.append('Future_Reef_MAP')
-        data_type = 'underway'
-        dir_list.extend([data_type, ship_callsign_ls[ship_code]])
-
-    if project in ['IMOS', 'FutureReefMap']:
-        att_list = FileClassifier._get_nc_att(src_file.src_path, ['cruise_id', 'time_coverage_start'])
-        year = att_list[1][:4]
-        cruise_id = att_list[0]
-        dir_list.extend([year, cruise_id])
-
-    if project == 'SOOP-CO2_RT':
-        data_type = 'REALTIME'
-        time_start = FileClassifier._get_nc_att(src_file.src_path, 'time_coverage_start')
-        year = time_start[:4]
-        month = time_start[5:7]
-        month = month.lstrip('0')
-        dir_list.extend([data_type, year, month])
-
-    return FileClassifier._make_path(dir_list)
-
-
-def archive_path_soop_co2(src_file):
-    """
-    Generate archive path for RT file based on vessel_code
-        eg:IN_2017-165-0000dat.txt
-          <Vessel_code>_yyyy-ddd-hhmmdat.txt
-    :return: relative archive path- full path, including file name
-    eg: 'IMOS/SOOP/SOOP-CO2/VLMJ_Investigator/REALTIME/2018/1/IN_2018-022-0000dat.txt'
-    """
-    dir_list = []
-    project = 'IMOS'
-    facility = 'SOOP'
-    sub_facility = 'SOOP-CO2'
-    data_type = 'REALTIME'
-    dir_list.extend([project, facility, sub_facility])
-    ship_callsign_ls = ship_callsign_list()
-    fields = FileClassifier._get_file_name_fields(os.path.basename(src_file), min_fields=2)
-    if fields[0] in VESSEL_CODE:
-        ship_code = VESSEL_CODE[fields[0]]
-    else:
-        raise InvalidFileNameError(
-            "File {file} has an invalid vessel code or is not a valid SOOP-CO2 realtime file".format(
-                file=os.path.basename(src_file))
-        )
-    platform = "{ship_code}_{ship_name}".format(ship_code=ship_code, ship_name=ship_callsign_ls[ship_code])
-    dir_list.extend([platform, data_type])
-    year = int(fields[1][:4])
-    dir_list.append(year)
-    jday = int(fields[1][5:8])
-    if not (jday in range(0, 366)) or year < 2017:
-        raise InvalidFileNameError(
-            "Failed extracting valid [year, day] from file {file}".format(file=os.path.basename(src_file))
-        )
-
-    # Determine month from julian day (1-365). Leap year taken into account
-    year_to_ordinal = datetime.date(year, 1, 1).toordinal() + jday - 1
-    month = datetime.date.fromordinal(year_to_ordinal).month
-    dir_list.append(month)
-    dir_list.append(os.path.basename(src_file))
-
-    return FileClassifier._make_path(dir_list)
 
 
 class SoopCo2Handler(HandlerBase):
@@ -143,12 +49,10 @@ class SoopCo2Handler(HandlerBase):
          * NetCDF;
          * TXT;
     """
-    dest_path = dest_path_soop_co2
-    archive_path = archive_path_soop_co2
-
     def __init__(self, *args, **kwargs):
         super(SoopCo2Handler, self).__init__(*args, **kwargs)
         self.allowed_extensions = ['.nc', '.txt', '.zip']
+        self.ship_callsign_ls = None
 
     def preprocess(self):
         """ Preprocessing for NRT and DM files
@@ -158,6 +62,11 @@ class SoopCo2Handler(HandlerBase):
              These files are not checked or harvested, but uploaded to S3
 
         """
+        if self.custom_params is not None and self.custom_params.get('ship_callsign_ls'):
+            self.ship_callsign_ls = self.custom_params['ship_callsign_ls']
+        else:
+            self.ship_callsign_ls = ship_callsign_list()
+
         # Delayed mode file submitted as a zip archive
         if self.file_extension == '.zip':
             nc_file = self.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)
@@ -168,38 +77,116 @@ class SoopCo2Handler(HandlerBase):
 
             # first process the NetCDF file to set the destination path for the file collection
             nc = nc_file[0]
-            set_nc_dest_path = dest_path_soop_co2(nc)
-            nc.dest_path = os.path.join(set_nc_dest_path, nc.name)
+            nc.dest_path = self.dest_path(nc.src_path)
+            nc_dir_path = os.path.dirname(nc.dest_path)
 
-            # SOOP-CO2 DM and FRMAP .txt,.pdf or/and .xml files. Set check type to NONEMPTY and publish type to UPLOAD_ONLY
+            # SOOP-CO2 DM and FRMAP .txt,.pdf or/and .xml files.
+            # Set check type to NONEMPTY and publish type to UPLOAD_ONLY
             non_nc_files = PipelineFileCollection(f for f in self.file_collection if f.file_type is not FileType.NETCDF)
             for non_nc in non_nc_files:
                 non_nc.check_type = PipelineFileCheckType.FORMAT_CHECK
                 non_nc.publish_type = PipelineFilePublishType.UPLOAD_ONLY
-                non_nc.dest_path = os.path.join(set_nc_dest_path, non_nc.name)
+                non_nc.dest_path = os.path.join(nc_dir_path, non_nc._name)
 
         elif self.input_file.endswith('dat.txt'):
-            # Single text file
-            # Realtime files (*dat.txt)
+            # Single text file Realtime files (*dat.txt)
             rt_file = self.file_collection[0]
             rt_file.publish_type = PipelineFilePublishType.ARCHIVE_ONLY
 
             nrt_nc_file_path = soop_co2_nrt_nc_generator.process_co2_rt(rt_file, self.products_dir)
             nrt_nc_file = PipelineFile(nrt_nc_file_path)
             self.file_collection.add(nrt_nc_file)
-            nrt_nc_file.check_type = PipelineFileCheckType.NC_COMPLIANCE_CHECK
             nrt_nc_file.publish_type = PipelineFilePublishType.HARVEST_UPLOAD
-            set_nc_dest_path = dest_path_soop_co2(nrt_nc_file)
-            nrt_nc_file.dest_path = os.path.join(set_nc_dest_path, nrt_nc_file.name)
 
-        elif self.file_type is FileType.NETCDF:
-            # single NetCDF file
-            nc = self.file_collection[0]
-            nc_dest_path = dest_path_soop_co2(nc)
-            nc.dest_path = os.path.join(nc_dest_path, nc.name)
+    def dest_path(self, src_file):
+        dir_list = []
+        project = def_project(src_file)
+        ship_callsign_ls = ship_callsign_list()
 
+        if project not in VALID_PROJECT:
+            raise InvalidFileNameError(
+                "Invalid project name '{project}'. "
+                "Project should be IMOS, SOOP-CO2_RT or Future_Reef_MAP".format(project=project))
+
+        if project in ['IMOS', 'SOOP-CO2_RT']:
+            fields = FileClassifier._get_file_name_fields(src_file)
+            ship_code = fields[4]
+            if ship_code not in ship_callsign_ls:
+                raise InvalidFileNameError(
+                    "Missing vessel callsign in file name '{name}'.".format(name=src_file))
+
+            project_base = 'IMOS'
+            facility = fields[1][:4]
+            sub_facility = fields[1]
+            platform = "{ship_code}_{ship_name}".format(ship_code=ship_code, ship_name=ship_callsign_ls[ship_code])
+            dir_list.extend([project_base, facility, sub_facility, platform])
+
+        if project == 'FutureReefMap':
+            fields = FileClassifier._get_file_name_fields(src_file, min_fields=5)
+            ship_code = fields[3]
+            if ship_code not in ship_callsign_ls:
+                raise InvalidFileNameError(
+                    "Missing vessel callsign in file name '{name}'.".format(name=src_file))
+
+            dir_list.append('Future_Reef_MAP')
+            data_type = 'underway'
+            dir_list.extend([data_type, ship_callsign_ls[ship_code]])
+
+        if project in ['IMOS', 'FutureReefMap']:
+            att_list = FileClassifier._get_nc_att(src_file, ['cruise_id', 'time_coverage_start'])
+            year = att_list[1][:4]
+            cruise_id = att_list[0]
+            dir_list.extend([year, cruise_id])
+
+        if project == 'SOOP-CO2_RT':
+            data_type = 'REALTIME'
+            time_start = FileClassifier._get_nc_att(src_file, 'time_coverage_start')
+            year = time_start[:4]
+            month = time_start[5:7]
+            month = month.lstrip('0')
+            dir_list.extend([data_type, year, month])
+
+        dir_path = FileClassifier._make_path(dir_list)
+        return os.path.join(dir_path, os.path.basename(src_file))
+
+    def archive_path(self, src_file):
+        """
+        Generate archive path for RT file based on vessel_code
+            eg:IN_2017-165-0000dat.txt
+              <Vessel_code>_yyyy-ddd-hhmmdat.txt
+        :return: relative archive path- full path, including file name
+        eg: 'IMOS/SOOP/SOOP-CO2/VLMJ_Investigator/REALTIME/2018/1/IN_2018-022-0000dat.txt'
+        """
+        dir_list = []
+        project = 'IMOS'
+        facility = 'SOOP'
+        sub_facility = 'SOOP-CO2'
+        data_type = 'REALTIME'
+        dir_list.extend([project, facility, sub_facility])
+        ship_callsign_ls = ship_callsign_list()
+        fields = FileClassifier._get_file_name_fields(os.path.basename(src_file), min_fields=2)
+        if fields[0] in VESSEL_CODE:
+            ship_code = VESSEL_CODE[fields[0]]
         else:
-            raise InvalidInputFileError(
-                "File format '{file}' is invalid. Should be a SOOP-CO2 realtime file (*dat.txt) " 
-                "or a Delayed Mode zip archive or NetCDF file.".format(file=self.input_file)
+            raise InvalidFileNameError(
+                "File {file} has an invalid vessel code or is not a valid SOOP-CO2 realtime file".format(
+                    file=os.path.basename(src_file))
             )
+        platform = "{ship_code}_{ship_name}".format(ship_code=ship_code, ship_name=ship_callsign_ls[ship_code])
+        dir_list.extend([platform, data_type])
+        year = int(fields[1][:4])
+        dir_list.append(year)
+        jday = int(fields[1][5:8])
+        if not (jday in range(0, 366)) or year < 2017:
+            raise InvalidFileNameError(
+                "Failed extracting valid [year, day] from file {file}".format(file=os.path.basename(src_file))
+            )
+
+        # Determine month from julian day (1-365). Leap year taken into account
+        year_to_ordinal = datetime.date(year, 1, 1).toordinal() + jday - 1
+        month = datetime.date.fromordinal(year_to_ordinal).month
+        dir_list.append(month)
+        dir_list.append(os.path.basename(src_file))
+        archive_file_path = FileClassifier._make_path(dir_list)
+
+        return archive_file_path
--- test_aodndata/soop/test_soop_co2.py
+++ test_aodndata/soop/test_soop_co2.py
@@ -1,23 +1,22 @@
 import os
 import unittest
-from aodncore.pipeline import PipelineFilePublishType, PipelineFileCheckType
+
+from aodncore.pipeline import FileType, PipelineFilePublishType, PipelineFileCheckType
 from aodncore.testlib import HandlerTestCase
+
 from aodndata.soop.soop_co2 import SoopCo2Handler
-from mock import patch
 
 TEST_ROOT = os.path.join(os.path.dirname(__file__))
 GOOD_NC = os.path.join(TEST_ROOT, 'IMOS_SOOP-CO2_GST_20170126T023510Z_VNAA_FV01.nc')
-# NOT_A_NETCDF = os.path.join(TEST_ROOT, 'IMOS_SOOP-CO2_GST_20170831T074045Z_VLMJ_FV01.nc')
-# GOOD_RT = os.path.join(TEST_ROOT, 'IN_2018-022-0000dat.txt')
 GOOD_FRMAP = os.path.join(TEST_ROOT, 'FutureReefMap_GST_20150518T124011Z_9V2768_FV01.nc')
 GOOD_ZIP = os.path.join(TEST_ROOT, 'IMOS_SOOP-CO2_GST_20170126T023510Z_VNAA_FV01.zip')
+GOOD_RT_IN_TXT = os.path.join(TEST_ROOT, 'IN_2018-022-0000dat.txt')
+GOOD_RT_AA_TXT = os.path.join(TEST_ROOT, 'AA_2019-032-0001dat.txt')
 
-# GOOD_RT = os.path.join(TEST_ROOT, 'IMOS_SOOP-CO2_GST_20180122T000135Z_VLMJ_FV00_END-20180123T000058Z.nc')
-
-
-def mock_ship_callsign_list():
-    return {'VNAA': 'Aurora-Australis',
-            '9V2768': 'RTM-Wakmatha'}
+ship_callsign_ls = {'VNAA': 'Aurora-Australis',
+                    '9V2768': 'RTM-Wakmatha',
+                    'VLMJ': 'Investigator'
+                    }
 
 
 class TestSoopCo2Handler(HandlerTestCase):
@@ -25,18 +24,16 @@ class TestSoopCo2Handler(HandlerTestCase):
        unittest.TestCase class). This provides some useful methods and properties to shortcut some common test
        scenarios.
     """
-
-    # This is a "boilerplate" method that must appear in each test case in order to correctly inherit from the HandlerTestCase class
     def setUp(self):
         # set the handler_class attribute to your handler (as imported above)
         self.handler_class = SoopCo2Handler
         super(TestSoopCo2Handler, self).setUp()
 
-    @patch("aodndata.soop.soop_co2.ship_callsign_list", side_effect=mock_ship_callsign_list)
-    def test_good_co2_file(self, mock_callsign):
-        # we expect this to succeed, so if the handler experiences an error, it is considered a
-        # "failed test"
-        handler = self.run_handler(GOOD_NC, check_params={'checks': ['cf']})
+    def test_good_co2_nc(self):
+        handler = self.run_handler(GOOD_NC,
+                                   check_params={'checks': ['cf']},
+                                   custom_params={'ship_callsign_ls': ship_callsign_ls}
+                                   )
         self.assertEqual(len(handler.file_collection), 1)
         f = handler.file_collection[0]
         self.assertEqual(f.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
@@ -45,28 +42,7 @@ class TestSoopCo2Handler(HandlerTestCase):
                          'IMOS/SOOP/SOOP-CO2/VNAA_Aurora-Australis/2017/AA1617_V3/IMOS_SOOP-CO2_GST_20170126T023510Z_VNAA_FV01.nc')
         self.assertTrue(f.is_stored)
 
-    # def test_good_co2rt_file(self):
-    #     # we expect this to succeed, so if the handler experiences an error, it is considered a
-    #     # "failed test"
-    #     handler = self.run_handler(GOOD_RT)
-    #
-    #     self.assertEqual(len(handler.file_collection), 1)
-    #     txt_file = handler.file_collection.filter_by_attribute_value('extension', '.txt')
-    #     nc_file = handler.file_collection.filter_by_attribute_value('extension', '.nc')
-    #
-    #     for t in txt_file:
-    #         self.assertEqual(t.publish_type, PipelineFilePublishType.ARCHIVE_ONLY)
-    #         self.assertEqual(t.archive_path,
-    #                          'IMOS/SOOP/SOOP-CO2/VLMJ_Investigator/REALTIME/2018/1/' + t.name)
-    #         self.assertTrue(t.is_archived)
-    #
-    #     for nc in nc_file:
-    #         self.assertEqual(nc.dest_path,
-    #                         'IMOS/SOOP/SOOP-CO2/VLMJ_Investigator/REALTIME/2018/1/' + nc.name)
-    #         self.assertTrue(nc.is_stored)
-
-    @patch("aodndata.soop.soop_co2.ship_callsign_list", side_effect=mock_ship_callsign_list)
-    def test_frmap_file(self, mock_callsign):
+    def test_frmap_file(self):
         # test future Reef Map processing
         handler = self.run_handler(GOOD_FRMAP)
         self.assertEqual(len(handler.file_collection), 1)
@@ -77,13 +53,11 @@ class TestSoopCo2Handler(HandlerTestCase):
                          'Future_Reef_MAP/underway/RTM-Wakmatha/2015/WK201505N/FutureReefMap_GST_20150518T124011Z_9V2768_FV01.nc')
         self.assertTrue(f.is_stored)
 
-    @patch("aodndata.soop.soop_co2.ship_callsign_list", side_effect=mock_ship_callsign_list)
-    def test_good_nc_zip(self, mock_callsign):
-
-        handler = self.handler_class(GOOD_ZIP,
-                                     check_params={'checks': ['cf']}
+    def test_good_nc_zip(self):
+        handler = self.run_handler(GOOD_ZIP,
+                                   check_params={'checks': ['cf']},
+                                   custom_params={'ship_callsign_ls': ship_callsign_ls}
                                      )
-        handler.run()
         self.assertEqual(len(handler.file_collection), 3)
 
         nc_files = handler.file_collection.filter_by_attribute_value('extension', '.nc')
@@ -103,10 +77,52 @@ class TestSoopCo2Handler(HandlerTestCase):
             self.assertEqual(f.check_type, PipelineFileCheckType.NONEMPTY_CHECK)
             self.assertEqual(f.publish_type, PipelineFilePublishType.UPLOAD_ONLY)
 
-    @patch("aodndata.soop.soop_co2.ship_callsign_list", side_effect=mock_ship_callsign_list)
-    def test_good_file_with_compliance_check(self, mock_callsign):
+    def test_good_file_with_compliance_check(self):
         # we also expect this to succeed, since the test file is known be CF compliant
         self.run_handler(GOOD_NC, check_params={'checks': ['cf']})
+
+    def test_good_rt_in_txt(self):
+        handler = self.run_handler(GOOD_RT_IN_TXT,
+                                   custom_params={'ship_callsign_ls': ship_callsign_ls},
+                                   check_params={'checks': ['cf', 'imos:1.4']})
+
+        f_txt = handler.file_collection.filter_by_attribute_value('extension', '.txt')[0]
+        self.assertEqual(f_txt.publish_type, PipelineFilePublishType.ARCHIVE_ONLY)
+
+        f_nc = handler.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)[0]
+        self.assertEqual(f_nc.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
+        self.assertEqual('IMOS/SOOP/SOOP-CO2/VLMJ_Investigator/REALTIME/2018/1/'
+                         'IMOS_SOOP-CO2_GST_20180122T000135Z_VLMJ_FV00_END-20180123T000058Z.nc',
+                         f_nc.dest_path)
+
+        self.assertEqual(f_txt.publish_type, PipelineFilePublishType.ARCHIVE_ONLY)
+        self.assertEqual(os.path.join('IMOS/SOOP/SOOP-CO2/VLMJ_Investigator/REALTIME/2018/1/',
+                                      os.path.basename(GOOD_RT_IN_TXT)),
+                         f_txt.archive_path)
+        self.assertTrue(f_nc.is_checked)
+        self.assertTrue(f_nc.is_stored)
+
+    def test_good_rt_aa_txt(self):
+        handler = self.run_handler(GOOD_RT_AA_TXT,
+                                   custom_params={'ship_callsign_ls': ship_callsign_ls},
+                                   check_params={'checks': ['cf', 'imos:1.4']})
+
+        f_txt = handler.file_collection.filter_by_attribute_value('extension', '.txt')[0]
+        self.assertEqual(f_txt.publish_type, PipelineFilePublishType.ARCHIVE_ONLY)
+
+        f_nc = handler.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)[0]
+        self.assertEqual(f_nc.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
+        self.assertEqual('IMOS/SOOP/SOOP-CO2/VNAA_Aurora-Australis/REALTIME/2019/2/'
+                         'IMOS_SOOP-CO2_GST_20190201T000131Z_VNAA_FV00_END-20190202T000030Z.nc',
+                         f_nc.dest_path)
+
+        self.assertEqual(f_txt.publish_type, PipelineFilePublishType.ARCHIVE_ONLY)
+        self.assertEqual(os.path.join('IMOS/SOOP/SOOP-CO2/VNAA_Aurora-Australis/REALTIME/2019/2/',
+                                      os.path.basename(GOOD_RT_AA_TXT)),
+                         f_txt.archive_path)
+
+        self.assertTrue(f_nc.is_checked)
+        self.assertTrue(f_nc.is_stored)
 
 
 if __name__ == '__main__':
