dbd86256b5c95eeee35c9c4b6863352b23e301b1
lbesnard <laurent.besnard@utas.edu.au>
2019-10-17 13:48:02 +1100

    SOOP vocab - handle case of new vessel having similar name as previous vessel, but new callsign

--- aodndata/soop/ship_callsign.py
+++ aodndata/soop/ship_callsign.py
@@ -31,11 +31,22 @@ def ship_callsign_list():
     platform_codes = platform_altlabels_per_preflabel('Vessel')
     platform_codes = {key: item.replace(' ', '-') for key, item in platform_codes.items()}
 
+    # Astrolabe case to remove the "L'" from its name
     if 'FHZI' in platform_codes:
         platform_codes['FHZI'] = 'Astrolabe'
 
     if 'FASB' in platform_codes:
         platform_codes['FASB'] = 'Astrolabe'
+
+    """ this section deals with vessels which have a different callsign, but a similar name. This is the case for new 
+        vessels replacing their older 'version'. In the vocabulary, in order to deal with this special case, the vessel name
+        is written as 'Vessel-Name-{callsign}'
+        Example: "Highland-Chief-{VROJ8}"
+        """
+    for callsign in platform_codes:
+        str_to_rm = '-{{{callsign}}}'.format(callsign=callsign)
+        if str_to_rm in platform_codes[callsign]:
+            platform_codes[callsign] = platform_codes[callsign].replace(str_to_rm, '')
 
     return platform_codes
 
