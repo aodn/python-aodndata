13abe7f0c4b88dac63108b2f0ca96ba41f80645a
mhidas <marty.hidas@utas.edu.au>
2020-05-06 14:28:12 +1000

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ PACKAGE_NAME = 'aodndata'
 
 setup(
     name=PACKAGE_NAME,
-    version='1.2.1',
+    version='1.3.0',
     packages=find_packages(exclude=PACKAGE_EXCLUDES),
     url='https://github.com/aodn',
     license='GPLv3',
