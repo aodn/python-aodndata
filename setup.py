b3f6e9abc84e11eed396f9a5a68c3c96acad5dc6
aodn-ci <info@aodn.org.au>
2020-06-22 00:30:04 +0000

    Bump version to 1.3.7

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ PACKAGE_NAME = 'aodndata'
 
 setup(
     name=PACKAGE_NAME,
-    version='1.3.6',
+    version='1.3.7',
     packages=find_packages(exclude=PACKAGE_EXCLUDES),
     url='https://github.com/aodn',
     license='GPLv3',
