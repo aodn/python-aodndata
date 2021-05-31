5863dc56ec88123d8bb13ac662274f3ae78195db
aodn-ci <info@aodn.org.au>
2021-05-31 06:21:38 +0000

    Bump version to 1.3.45

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ PACKAGE_NAME = 'aodndata'
 
 setup(
     name=PACKAGE_NAME,
-    version='1.3.44',
+    version='1.3.45',
     packages=find_packages(exclude=PACKAGE_EXCLUDES),
     url='https://github.com/aodn',
     license='GPLv3',
