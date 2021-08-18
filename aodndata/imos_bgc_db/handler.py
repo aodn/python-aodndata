729732d51e8e6aa46498494cc2a02c24332ddb1d
mhidas <marty.hidas@utas.edu.au>
2021-08-18 16:13:38 +1000

    ImosBgcDbHandler to archive the input_file and only harvest the contents

--- aodndata/imos_bgc_db/handler.py
+++ aodndata/imos_bgc_db/handler.py
@@ -1,6 +1,7 @@
+from datetime import datetime, timezone
 import os
 
-from aodncore.pipeline import HandlerBase, FileType, PipelineFileCheckType
+from aodncore.pipeline import HandlerBase, FileType, PipelineFileCheckType, PipelineFilePublishType
 
 
 class ImosBgcDbHandler(HandlerBase):
@@ -9,18 +10,24 @@ class ImosBgcDbHandler(HandlerBase):
         self.allowed_extensions = ['.zip', '.csv']
         self.include_regexes = [".*\\.csv$"]
         self.harvest_type = 'csv'
+        self.archive_input_file = True
+        self.input_file_archive_path = self.dest_path(self.input_file)
 
     def preprocess(self):
-        """Set the check type to TableSchema check for all CSV files."""
-        self.file_collection \
-            .filter_by_attribute_id('file_type', FileType.CSV) \
-            .set_check_types(PipelineFileCheckType.TABLE_SCHEMA_CHECK)
+        """
+        Set up CSV files to be checked against TableSchema and harvested.
+        They don't need to be uploaded as the entire zip (input_file) will be archived.
+        """
+        csv_files = self.file_collection.filter_by_attribute_id('file_type', FileType.CSV)
+        csv_files.set_check_types(PipelineFileCheckType.TABLE_SCHEMA_CHECK)
+        csv_files.set_publish_types(PipelineFilePublishType.HARVEST_ONLY)
 
     def dest_path(self, filepath):
         """
-        Destination object path for the BGC test .zip file or the individual .csv files.
+        Destination/archive path function.
         """
-
-        base_path = "IMOS/BGC"
-        dest_path = os.path.join(base_path, os.path.basename(filepath))
+        base_path = "IMOS/BGC_DB"
+        timestamp = datetime.now(timezone.utc).strftime('.%Y%m%dT%H%M%SZ')
+        basename, ext = os.path.splitext(os.path.basename(filepath))
+        dest_path = os.path.join(base_path, basename + timestamp + ext)
         return dest_path
