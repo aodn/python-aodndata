085bcf99d44e1a5fd01c6b6ccca1be4c3c6e021b
mhidas <marty.hidas@utas.edu.au>
2021-11-03 16:44:25 +1100

    add more test cases

--- test_aodndata/imos_bgc_db/test_imosBgcDbHandler.py
+++ test_aodndata/imos_bgc_db/test_imosBgcDbHandler.py
@@ -1,9 +1,11 @@
 import os
 import unittest
+from shutil import copyfile
 from unittest.mock import patch
 
-from aodncore.pipeline.configlib import LazyConfigManager
 from aodncore.pipeline import (PipelineFileCheckType, PipelineFilePublishType, FileType)
+from aodncore.pipeline.configlib import LazyConfigManager
+from aodncore.pipeline.exceptions import ComplianceCheckFailedError
 from aodncore.testlib import HandlerTestCase
 from aodncore.testlib.testutil import load_runtime_patched_pipeline_config_file, TESTLIB_CONF_DIR
 
@@ -22,10 +24,6 @@ HARVEST_PARAMS = {
             "type": "table"
         },
         {
-            "name": "bgc_phyto_raw",
-            "type": "table"
-        },
-        {
             "name": "bgc_phytoplankton_map",
             "type": "materialized view",
             "dependencies": ["bgc_trip"]
@@ -35,6 +33,7 @@ HARVEST_PARAMS = {
 
 TEST_ROOT = os.path.dirname(__file__)
 GOOD_CSV = os.path.join(TEST_ROOT, 'bgc_trip.csv')
+GOOD_ZIP = os.path.join(TEST_ROOT, 'bgc_phyto_test.zip')
 
 
 class TestImosBgcDbHandler(HandlerTestCase):
@@ -60,6 +59,7 @@ class TestImosBgcDbHandler(HandlerTestCase):
 
         self.assertEqual(handler.harvest_type, 'csv')
         self.assertTrue(handler.input_file_object.is_archived)
+        self.assertEqual(len(handler.file_collection), 1)
         for f in handler.file_collection.filter_by_attribute_id('file_type', FileType.CSV):
             self.assertIs(f.check_type, PipelineFileCheckType.TABLE_SCHEMA_CHECK)
             self.assertIs(f.publish_type, PipelineFilePublishType.HARVEST_ONLY)
@@ -67,6 +67,37 @@ class TestImosBgcDbHandler(HandlerTestCase):
             self.assertFalse(f.is_stored)
 
         self.assertTrue(mock_harvester.called)
+
+    @patch('aodncore.pipeline.steps.harvest.CsvHarvesterRunner')
+    def test_good_zip(self, mock_harvester):
+        handler = self.run_handler(GOOD_ZIP, harvest_params=HARVEST_PARAMS, config=self.testconfig)
+
+        self.assertEqual(handler.harvest_type, 'csv')
+        self.assertTrue(handler.input_file_object.is_archived)
+        self.assertEqual(len(handler.file_collection), 2)
+        for f in handler.file_collection.filter_by_attribute_id('file_type', FileType.CSV):
+            self.assertIs(f.check_type, PipelineFileCheckType.TABLE_SCHEMA_CHECK)
+            self.assertIs(f.publish_type, PipelineFilePublishType.HARVEST_ONLY)
+            self.assertTrue(f.is_checked)
+            self.assertFalse(f.is_stored)
+
+        self.assertTrue(mock_harvester.called)
+
+    def test_unmatched_csv(self):
+        unmatched_csv = os.path.join(self.temp_dir, 'unmatched.csv')
+        copyfile(GOOD_CSV, unmatched_csv)
+
+        self.run_handler_with_exception(ComplianceCheckFailedError, unmatched_csv,
+                                        harvest_params=HARVEST_PARAMS, config=self.testconfig
+                                        )
+
+    def test_invalid_csv(self):
+        invalid_csv = os.path.join(self.temp_dir, 'bgc_trip.csv')
+        copyfile(os.path.join(TEST_ROOT, 'bgc_trip_bad.csv'), invalid_csv)
+
+        self.run_handler_with_exception(ComplianceCheckFailedError, invalid_csv,
+                                        harvest_params=HARVEST_PARAMS, config=self.testconfig
+                                        )
 
 
 if __name__ == '__main__':
