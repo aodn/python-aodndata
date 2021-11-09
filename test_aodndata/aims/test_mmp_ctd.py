5e176d63b6f6264b0cdd8aacbeba36aff84a869b
lbesnard <laurent.besnard@utas.edu.au>
2021-11-09 11:07:03 +1100

    MMP CTD - update path as asked by data uploader

--- test_aodndata/aims/test_mmp_ctd.py
+++ test_aodndata/aims/test_mmp_ctd.py
@@ -10,7 +10,7 @@ class TestMmpCtd(unittest.TestCase):
 
     def test_dest_path_mmp_ctd(self):
         good_nc = os.path.join(TEST_ROOT, 'AIMS_MMP-WQ_20061027T111208Z_WQN083_FV01_Profile-SBE19plus_C-20210831T061352Z.nc')
-        self.assertEqual('AIMS/Marine_Monitoring_Program/BUR4/Biogeochem_profiles/2006/AIMS_MMP-WQ_20061027T111208Z_WQN083_FV01_Profile-SBE19plus_C-20210831T061352Z.nc',
+        self.assertEqual('AIMS/Marine_Monitoring_Program/CTD_profiles/2006/BUR4/AIMS_MMP-WQ_20061027T111208Z_WQN083_FV01_Profile-SBE19plus_C-20210831T061352Z.nc',
                          dest_path_mmp_ctd(good_nc))
 
 
