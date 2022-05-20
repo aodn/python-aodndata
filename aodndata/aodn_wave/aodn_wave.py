74c993e42cd1d76f4070b3ef7d5076dcf305ee8c
bpasquer <benedicte.pasquer@utas.edu.au>
2022-05-20 16:02:19 +1000

    process realtime mode + unittests

--- aodndata/aodn_wave/aodn_wave.py
+++ aodndata/aodn_wave/aodn_wave.py
@@ -9,10 +9,9 @@ BOM_DIR = 'Bureau_of_Meteorology'
 DOT_WA_DIR = 'Department_of_Transport-Western_Australia'
 DTA_NZ_DIR = 'Defence_Technology_Agency-New_Zealand'
 DES_QLD_DIR = 'Department_of_Environment_and_Science-Queensland'
-MHL_DIR_BASE = 'NSW-OEH'
 MHL_DIR = 'Manly_Hydraulics_Laboratory'
 NTP_WAVE_DIR = 'IMOS/NTP/Low_Cost_Wave_Buoy_Technology'
-NSW_DPE_DIR = 'NSW-DPE'
+NSW_DPE_DIR = 'Department_of_Planning_and_Environment-New_South_Wales'
 VIC_DEAKIN_DIR = 'Deakin_University'
 UWA_DIR = 'UWA'
 
@@ -20,117 +19,131 @@ WAVEBUOY_DIR = 'Wave_Buoys'
 DELAYED_DIR = 'Delayed'
 REALTIME_DIR = 'Realtime'
 
-
+DATA_MODE_REGEX = re.compile(r"""
+                           (.*)_
+                           (?P<nc_time_cov_start>[0-9]{8})_
+                           (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                           (?P<nc_time_cov_end>[0-9]{8})\.nc$
+                           """, re.VERBOSE)
 BOM_WAVEBUOY = re.compile(r"""
                            BOM_
-                           (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                           (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-
-                           (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                           (?P<nc_time_cov_start>[0-9]{8})_
+                           (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                           (?P<nc_time_cov_end>[0-9]{8})\.nc$
                            """, re.VERBOSE)
 
 DOT_WA_WAVEBUOY = re.compile(r"""
                               DOT-WA_
-                              (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                              (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-                               
-                              (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                              (?P<nc_time_cov_start>[0-9]{8})_
+                              (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-                               
+                              (?P<nc_time_cov_end>[0-9]{8})\.nc$
                               """, re.VERBOSE)
 
 DTA_NZ_WAVEBUOY = re.compile(r"""
                               DTA_
-                              (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                              (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-                               
-                              (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                              (?P<nc_time_cov_start>[0-9]{8})_
+                              (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-                               
+                              (?P<nc_time_cov_end>[0-9]{8})\.nc$
                               """, re.VERBOSE)
 
 DES_QLD_WAVEBUOY = re.compile(r"""
                                DES-QLD_
-                               (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                               (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-
-                               (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                               (?P<nc_time_cov_start>[0-9]{8})_
+                               (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                               (?P<nc_time_cov_end>[0-9]{8})\.nc$
                                """, re.VERBOSE)
 
 MHL_WAVEBUOY = re.compile(r"""
-                               IMOS_ANMN-NSW_W_
-                               (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                               (?P<site_code>(.*))_WAVERIDER_FV01_END-
-                               (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                               IMOS_ANMN-NSW_
+                               (?P<nc_time_cov_start>[0-9]{8})_
+                               (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                               (?P<nc_time_cov_end>[0-9]{8})\.nc$
                                """, re.VERBOSE)
 
 NTP_WAVEBUOY = re.compile(r"""
-                               IMOS_NTP-WAVE_(TW|W)_
-                               (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                               (?P<site_code>(.*))_WAVERIDER_FV01_timeseries_END-
-                               (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                               IMOS_NTP-WAVE_
+                               (?P<nc_time_cov_start>[0-9]{8})_
+                               (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                               (?P<nc_time_cov_end>[0-9]{8})\.nc$
                                """, re.VERBOSE)
 
 NSW_DPE_WAVEBUOY =  re.compile(r"""
                                NSW-DPE_
-                               (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                               (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-
-                               (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                               (?P<nc_time_cov_start>[0-9]{8})_
+                               (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                               (?P<nc_time_cov_end>[0-9]{8})\.nc$
                                """, re.VERBOSE)
 
 VIC_DEAKIN_WAVEBUOY =  re.compile(r"""
                                VIC-DEAKIN-UNI_
-                               (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                               (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-
-                               (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                               (?P<nc_time_cov_start>[0-9]{8})_
+                               (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                               (?P<nc_time_cov_end>[0-9]{8})\.nc$
                                """, re.VERBOSE)
 
 UWA_WAVEBUOY = re.compile(r"""
                                UWA_
-                               (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_
-                               (?P<site_name>(.*))_DM_WAVE-PARAMETERS_END-
-                               (?P<nc_time_cov_end>[0-9]{8}T[0-9]{6}Z)\.nc$
+                               (?P<nc_time_cov_start>[0-9]{8})_
+                               (?P<site_name>(.*))_(?P<mode>RT|DM)_WAVE-PARAMETERS_END-
+                               (?P<nc_time_cov_end>[0-9]{8})\.nc$
                                """, re.VERBOSE)
 
-def dest_path_aodn_wave_dm(filepath):
+def dest_path_aodn_wave(filepath):
     file_basename = os.path.basename(filepath)
-    with Dataset(filepath, mode='r') as nc_obj:
-        site_name = nc_obj.site_name
 
-    if BOM_WAVERIDER.match(file_basename):
-        data_base_dir = os.path.join(DES_QLD_DIR, WAVEBUOY_DIR, DELAYED_DIR)
-        fields = get_pattern_subgroups_from_string(file_basename, DES_QLD_WAVEBUOY)
+    fields = get_pattern_subgroups_from_string(file_basename, DATA_MODE_REGEX)
+    mode = fields['mode']
+    if mode == 'RT':
+        mode_dir = REALTIME_DIR
+    elif mode == 'DM':
+        mode_dir = DELAYED_DIR
+    else:
+        raise InvalidFileNameError(
+            "file name: \"{filename}\" data mode (RT/DM) missing or incorrect".format(
+                filename=file_basename))
+
+    if BOM_WAVEBUOY.match(file_basename):
+        data_base_dir = os.path.join(BOM_DIR, WAVEBUOY_DIR, mode_dir)
+        fields = get_pattern_subgroups_from_string(file_basename, BOM_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif DOT_WA_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(DOT_WA_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(DOT_WA_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, DOT_WA_WAVEBUOY)
         product_dir = fields['site_name']
 
-    elif DTA_NZ.match(file_basename):
-        data_base_dir = os.path.join(DTA_NZ_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+    elif DTA_NZ_WAVEBUOY.match(file_basename):
+        data_base_dir = os.path.join(DTA_NZ_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, DTA_NZ_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif DES_QLD_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(DES_QLD_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(DES_QLD_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, DES_QLD_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif MHL_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(MHL_DIR_BASE, MHL_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(NSW_DPE_DIR, MHL_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, MHL_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif NTP_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(NTP_WAVE_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(NTP_WAVE_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, NTP_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif NSW_DPE_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(NSW_DPE_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(NSW_DPE_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, NSW_DPE_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif VIC_DEAKIN_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(VIC_DEAKIN_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(VIC_DEAKIN_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, VIC_DEAKIN_WAVEBUOY)
         product_dir = fields['site_name']
 
     elif UWA_WAVEBUOY.match(file_basename):
-        data_base_dir = os.path.join(UWA_DIR, WAVEBUOY_DIR, DELAYED_DIR)
+        data_base_dir = os.path.join(UWA_DIR, WAVEBUOY_DIR, mode_dir)
         fields = get_pattern_subgroups_from_string(file_basename, UWA_WAVEBUOY)
         product_dir = fields['site_name']
 
---
+++ test_aodndata/aodn_wave/test_aodn_wave.py
@@ -0,0 +1,158 @@
+import os
+import unittest
+import shutil
+from tempfile import mkdtemp
+
+from aodncore.pipeline.exceptions import InvalidFileNameError, InvalidFileContentError
+from aodncore.testlib import make_test_file
+from aodndata.aodn_wave.aodn_wave import dest_path_aodn_wave
+
+TEST_ROOT = os.path.join(os.path.dirname(__file__))
+
+
+class TestAodnWaveHandler(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def test_dest_path_aodn_wave(self):
+        filename = 'NSW-DPE_20160811_MAROUBRA_DM_WAVE-PARAMETERS_END-20160912.nc'
+        testfile = os.path.join(self.tempdir, filename)
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Department_of_Planning_and_Environment-New_South_Wales',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'MAROUBRA',
+                             os.path.basename(testfile)))
+
+        testfile = 'DOT-WA_20170601_CAPE-NATURALISTE_DM_WAVE-PARAMETERS_END-20170918.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Department_of_Transport-Western_Australia',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'CAPE-NATURALISTE',
+                             os.path.basename(testfile)))
+
+        testfile = 'BOM_19980107_CAPE-SORELL_DM_WAVE-PARAMETERS_END-19981231.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Bureau_of_Meteorology',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'CAPE-SORELL',
+                             os.path.basename(testfile)))
+
+        testfile = 'BOM_19980107_CAPE-DU-COUEDIC_RT_WAVE-PARAMETERS_END-19981231.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Bureau_of_Meteorology',
+                             'Wave_Buoys',
+                             'Realtime',
+                             'CAPE-DU-COUEDIC',
+                             os.path.basename(testfile)))
+
+
+
+        testfile = 'DES-QLD_19750918_MACKAY_RT_WAVE-PARAMETERS_END-20110210.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Department_of_Environment_and_Science-Queensland',
+                             'Wave_Buoys',
+                             'Realtime',
+                             'MACKAY',
+                             os.path.basename(testfile)))
+
+        testfile = 'IMOS_ANMN-NSW_20140529_BYRON_BAY_DM_WAVE-PARAMETERS_END-20151231.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Department_of_Planning_and_Environment-New_South_Wales',
+                             'Manly_Hydraulics_Laboratory',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'BYRON_BAY',
+                             os.path.basename(testfile)))
+
+        testfile = 'DTA_20190531_CAMPBELL-ISLAND_DM_WAVE-PARAMETERS_END-20190531.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Defence_Technology_Agency-New_Zealand',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'CAMPBELL-ISLAND',
+                             os.path.basename(testfile)))
+
+        testfile = 'DTA_20170530_SOUTHERN-OCEAN_DM_WAVE-PARAMETERS_END-20170530.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'Defence_Technology_Agency-New_Zealand',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'SOUTHERN-OCEAN',
+                             os.path.basename(testfile)))
+
+        testfile = 'IMOS_NTP-WAVE_20200113_TORBAY_DM_WAVE-PARAMETERS_END-20200319.nc'
+        make_test_file(testfile,{'site_name': 'Maroubra'} ,
+                       WSSH={}
+                       )
+        dest_dir= dest_path_aodn_wave(testfile)
+        self.assertEqual(dest_dir,
+                         os.path.join(
+                             'IMOS/NTP/Low_Cost_Wave_Buoy_Technology',
+                             'Wave_Buoys',
+                             'Delayed',
+                             'TORBAY',
+                             os.path.basename(testfile)))
+
+     #   badmode = 'IMOS_NTP-WAVE_20200113_TORBAY_RM_WAVE-PARAMETERS_END-20200319.nc'
+      #  with self.assertRaises(InvalidFileNameError):
+       #     dest_path_aodn_wave_dm(badmode)
+
+   #     badplatform = 'BOM_W_20001129_CAMPBELL_WAVE-PARAMETERS_END-20001231.nc'
+   #     make_test_file(testfile, {'site_name': 'Maroubra'},
+   #                    WSSH={}
+   #                    )
+    #    dest_dir = dest_path_aodn_wave(badplatform)
+   #     with self.assertRaises(InvalidFileNameError):
+    #        dest_path_aodn_wave_dm(badplatform)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
