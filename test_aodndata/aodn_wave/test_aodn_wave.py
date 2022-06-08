bcbb6a707eea3a21ed39becb6f660e425a05a69e
ana-berger <ana.berger@utas.edu.au>
2022-06-08 17:09:44 +1000

    wip unit_tests

--- test_aodndata/aodn_wave/test_aodn_wave.py
+++ test_aodndata/aodn_wave/test_aodn_wave.py
@@ -3,41 +3,31 @@ import unittest
 import shutil
 from tempfile import mkdtemp
 
-from aodncore.pipeline.exceptions import InvalidFileNameError, InvalidFileContentError
-from aodncore.testlib import make_test_file
+from aodncore.pipeline import FileType, PipelineFilePublishType
+from aodncore.pipeline.exceptions import InvalidFileNameError, InvalidFileContentError, InvalidInputFileError
+from aodncore.testlib import make_test_file, handlertest, HandlerTestCase
+from aodndata.aodn_wave import handler
 from aodndata.aodn_wave.handler import AodnWaveHandler
 
 TEST_ROOT = os.path.join(os.path.dirname(__file__))
 
-PARAMETERS_FILE = os.path.join(TEST_ROOT, 'WAVE-PARAMETERS_END-12345678.nc')
+PARAMETERS_FILE = os.path.join(TEST_ROOT, 'NSW-DPE_20160811_MAROUBRA_DM_WAVE-PARAMETERS_END-20160912.nc')
 SPECTRA_FILE = os.path.join(TEST_ROOT, 'SPECTRA_END-12345678.nc')
 RAWDISPL_FILE = os.path.join(TEST_ROOT, 'RAW-DISPLACEMENTS_END-12345678.nc')
 
 
-class TestAodnWaveHandler(unittest.TestCase):
+class TestAodnWaveHandler(HandlerTestCase):
 
     def setUp(self):
         self.tempdir = mkdtemp()
+        self.handler_class = AodnWaveHandler
+
+        super(TestAodnWaveHandler, self).setUp()
 
     def tearDown(self):
         shutil.rmtree(self.tempdir)
 
     def test_dest_path(self):
-        filename = 'NSW-DPE_20160811_MAROUBRA_DM_WAVE-PARAMETERS_END-20160912.nc'
-        testfile = os.path.join(self.tempdir, filename)
-        make_test_file(testfile, {'site_name': 'Maroubra'},
-                       WSSH={}
-                       )
-        dest_dir = AodnWaveHandler.dest_path(testfile)
-        self.assertEqual(dest_dir,
-                         os.path.join(
-                             'Department_of_Planning_and_Environment-New_South_Wales',
-                             'Wave_Buoys',
-                             'DELAYED',
-                             'WAVE-PARAMETERS',
-                             'MAROUBRA',
-                             os.path.basename(testfile)))
-
         testfile = 'DOT-WA_20170601_CAPE-NATURALISTE_DM_WAVE-PARAMETERS_END-20170918.nc'
         make_test_file(testfile, {'site_name': 'Maroubra'},
                        WSSH={}
@@ -46,7 +36,7 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Department_of_Transport-Western_Australia',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'DELAYED',
                              'WAVE-PARAMETERS',
                              'CAPE-NATURALISTE',
@@ -60,7 +50,7 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Bureau_of_Meteorology',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'DELAYED',
                              'WAVE-PARAMETERS',
                              'CAPE-SORELL',
@@ -74,7 +64,7 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Bureau_of_Meteorology',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'REALTIME',
                              'WAVE-PARAMETERS',
                              'CAPE-DU-COUEDIC',
@@ -88,26 +78,11 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Department_of_Environment_and_Science-Queensland',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'REALTIME',
                              'WAVE-PARAMETERS',
                              'MACKAY',
                              os.path.basename(testfile)))
-
-        # testfile = 'IMOS_ANMN-NSW_20140529_BYRON_BAY_DM_WAVE-PARAMETERS_END-20151231.nc'
-        # make_test_file(testfile, {'site_name': 'Maroubra'},
-        #                WSSH={}
-        #                )
-        # dest_dir = AodnWaveHandler.dest_path(testfile)
-        # self.assertEqual(dest_dir,
-        #                  os.path.join(
-        #                      'Department_of_Planning_and_Environment-New_South_Wales',
-        #                      'Manly_Hydraulics_Laboratory',
-        #                      'Wave_Buoys',
-        #                      'DELAYED',
-        #                      'WAVE-PARAMETERS',
-        #                      'BYRON_BAY',
-        #                      os.path.basename(testfile)))
 
         testfile = 'DTA_20190531_CAMPBELL-ISLAND_DM_WAVE-PARAMETERS_END-20190531.nc'
         make_test_file(testfile, {'site_name': 'Maroubra'},
@@ -117,7 +92,7 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Defence_Technology_Agency-New_Zealand',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'DELAYED',
                              'WAVE-PARAMETERS',
                              'CAMPBELL-ISLAND',
@@ -131,7 +106,7 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Defence_Technology_Agency-New_Zealand',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'DELAYED',
                              'WAVE-PARAMETERS',
                              'SOUTHERN-OCEAN',
@@ -145,7 +120,7 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'IMOS/NTP/Low_Cost_Wave_Buoy_Technology',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'DELAYED',
                              'WAVE-PARAMETERS',
                              'TORBAY',
@@ -159,24 +134,12 @@ class TestAodnWaveHandler(unittest.TestCase):
         self.assertEqual(dest_dir,
                          os.path.join(
                              'Department_of_Planning_and_Environment-New_South_Wales',
-                             'Wave_Buoys',
+                             'WAVE-BUOYS',
                              'DELAYED',
                              'SPECTRA',
                              'MAROUBRA',
                              os.path.basename(testfile)))
 
-    #   badmode = 'IMOS_NTP-WAVE_20200113_TORBAY_RM_WAVE-PARAMETERS_END-20200319.nc'
-    #  with self.assertRaises(InvalidFileNameError):
-    #     dest_path_aodn_wave_dm(badmode)
-
-
-#     badplatform = 'BOM_W_20001129_CAMPBELL_WAVE-PARAMETERS_END-20001231.nc'
-#     make_test_file(testfile, {'site_name': 'Maroubra'},
-#                    WSSH={}
-#                    )
-#    dest_dir = dest_path_aodn_wave(badplatform)
-#     with self.assertRaises(InvalidFileNameError):
-#        dest_path_aodn_wave_dm(badplatform)
 
 
 if __name__ == '__main__':
