7e8d493798bb8117b19476170d9aff756cc402ff
mhidas <marty.hidas@utas.edu.au>
2023-01-17 17:25:33 +1100

    WIP: moorings products trigger taking variables into account

--- aodndata/moorings/moorings_product_trigger.py
+++ aodndata/moorings/moorings_product_trigger.py
@@ -10,7 +10,7 @@ from owslib.fes import PropertyIsEqualTo, PropertyIsNotEqualTo, And, Or
 
 import pandas as pd
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # Variables included in the products
 INCLUDED_VARIABLES = {'TEMP', 'PSAL', 'CPHL', 'CHLF', 'CHLU', 'TURB', 'DOX1', 'DOX2', 'DOXS', 'PAR', 'VCUR'}
@@ -60,13 +60,12 @@ def pivot_variables(df: pd.DataFrame) -> pd.DataFrame:
     assert 'variables' in df.columns
     files_vars = []
     for row in df.itertuples():
-              variables = set(row.variables.split(', ')) & INCLUDED_VARIABLES
-              for v in variables:
-                  files_vars.append((row.url, v))
+          variables = set(row.variables.split(', ')) & INCLUDED_VARIABLES
+          files_vars.extend((row.Index, v) for v in variables)
 
-    files_vars = pd.DataFrame(files_vars, columns=['url', 'variable']).set_index('url')
+    files_vars = pd.DataFrame(files_vars, columns=['i', 'variable']).set_index('i')
 
-    return df.drop(columns='variables').join(files_vars, on='url')
+    return df.drop(columns='variables').join(files_vars)
 
 
 def files_for_site(site_code: str) -> pd.DataFrame:
@@ -94,31 +93,36 @@ def files_for_site(site_code: str) -> pd.DataFrame:
 
 
 # Get all relevant FV01 & FV02 files for site
-site_code = 'NRSMAI'
+site_code = 'GBRLSH'
 files_df = files_for_site(site_code)
 
 # source files
 source_index = np.logical_and(files_df.file_version == 1,
                               files_df.data_category.map(lambda s: s not in ('aggregated_timeseries', 'CO2'))
                               )
-source_files = files_df.loc[source_index, ['url', 'date_updated', 'variable']].set_index('url')
-logging.info(f"Found {len(source_files)} source files")
+source_files = files_df.loc[source_index, ['url', 'date_updated', 'variable']]
+logging.info(f"Found {len(source_files.url.unique())} source files")
 
 # product_files
-aggregated_files = files_df.loc[files_df.data_category=='aggregated_timeseries', ['url', 'date_created']]
-logging.debug(f"Found {len(aggregated_files)} aggregated products")
+aggregated_files = files_df.loc[files_df.data_category=='aggregated_timeseries', ['url', 'date_created', 'variable']]
 hourly_files = files_df.loc[files_df.data_category=='hourly_timeseries', ['url', 'date_created']]
-logging.debug(f"Found {len(hourly_files)} hourly products")
 
-# when were any of the products last updated? - assume they were all up to date at that time
-products_updated = max(aggregated_files.date_created.max(), hourly_files.date_created.max())
-logging.info(f"Products last updated {products_updated}")
+# when were data for each variable updated?
+vars_updated = source_files.groupby('variable').date_updated.max()
+vars_updated.name = 'source_updated'
+aggregated_files = aggregated_files.join(vars_updated, on='variable', how='right')
 
-# what source files have changed since then and what variables were included?
-new_source_files = source_files[source_files.date_updated > products_updated]
-new_vars = set(new_source_files.variable)
+# which variables have newer data than the product, or missing product
+new_vars = aggregated_files[np.logical_or(aggregated_files.date_created < aggregated_files.source_updated,
+                                          aggregated_files.url.isna()
+                                          )]
+new_vars = set(new_vars.variable)
 
-if len(new_vars) == 0:
+# what source files have changed since the hourly products were last created?
+products_updated = hourly_files.date_created.max()
+new_source_hourly = source_files[source_files.date_updated > products_updated]
+
+if len(new_vars) == 0 and new_source_hourly.empty:
     logging.info(f"No new data for site {site_code}")
     exit(0)
 
@@ -129,7 +133,9 @@ if 'VCUR' in new_vars:
     new_vars.remove('VCUR')
     products.update({'velocity_aggregated', 'velocity_hourly'})
 if new_vars:
-    products.update({'aggregated', 'hourly', 'gridded'})
+    products.add('aggregated')
+if not new_source_hourly.empty:
+    products.update({'hourly', 'gridded'})
 
 # create manifest
 manifest = {'site_code': site_code,
