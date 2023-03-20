0562b8d7aede38f79c8cbedf38aeba0e04f0686b
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2023-03-20 12:40:05 +1100

    update handler and tests to ingest .nc files only

--- aodndata/gsla/handler.py
+++ aodndata/gsla/handler.py
@@ -13,13 +13,13 @@ GSLA_PREFIX_PATH = "IMOS/OceanCurrent/GSLA"
 GSLA_REGEX = re.compile(r"""
                        IMOS_OceanCurrent_HV_
                        (?P<nc_time_cov_start>[0-9]{8}T[0-9]{6}Z)_GSLA_FV02_
-                       (?P<product_type>NRT|DM0[1-2])\.nc(\.gz)?$
+                       (?P<product_type>NRT|DM0[1-2])\.nc$
                        """, re.VERBOSE)
 
 GSLA_REGEX_YEARLY = re.compile(r"""
                                IMOS_OceanCurrent_HV_
                                (?P<product_type>|DM01_)  # DM01 yearly file have their product code within the file.
-                               (?P<nc_time_cov_start>[0-9]{4})\.nc(\.gz)?$
+                               (?P<nc_time_cov_start>[0-9]{4})\.nc$
                                """, re.VERBOSE)
 
 
@@ -44,58 +44,33 @@ def get_product_type(netcdf_path):
             return nc_obj.product_type
         except AttributeError:
             raise InvalidInputFileError(
-                "Expecting 'product_type' attribute in netCDF'{gzip}'".format(gzip=os.path.basename(netcdf_path))
+                "Expecting 'product_type' attribute in netCDF'{ncfile}'".format(ncfile=os.path.basename(netcdf_path))
             )
 
 class GslaHandler(HandlerBase):
     def __init__(self, *args, **kwargs):
         super(GslaHandler, self).__init__(*args, **kwargs)
-        self.allowed_extensions = ['.gz', '.nc']
+        self.allowed_extensions = ['.nc']
 
     def preprocess(self):
 
-        # if input file is a NetCDF, create a .nc.gz and harvest upload it.
-        # historically, files were always sent as *.nc.gz. But as of April 2021, files might be pushed as *.nc.
-        # to be consistent, we transform this .nc into a .nz.gz
-        if self.file_type is FileType.NETCDF:
-            self.file_collection.set_publish_types(PipelineFilePublishType.NO_ACTION)
+        # historically, files were always sent as *.nc.gz. As of April 2021, files could be pushed as *.nc as well.
+        # from March 2023, as part of the whole collection reprocessing, files will be pushed only as *.nc
 
-            gzip_path = os.path.join(self.temp_dir,  self.file_basename + '.gz')
-            with open(self.input_file, 'rb') as f_in, gzip.open(gzip_path, 'wb') as gz_out:
-                gz_out.writelines(f_in)
+        netcdf_file_collection = self.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)
+        netcdf_file = netcdf_file_collection[0]
 
-            # publish
-            self.add_to_collection(gzip_path, publish_type=PipelineFilePublishType.HARVEST_UPLOAD)
-
-        if self.file_type is FileType.GZIP:
-            # add nc_gz file to collection (not by default)
-            self.file_collection.add(self.input_file_object)
-            netcdf_file_gz_collection = self.file_collection.filter_by_attribute_id('file_type', FileType.GZIP)
-            netcdf_file_gz = netcdf_file_gz_collection[0]
-            netcdf_file_gz.publish_type = PipelineFilePublishType.HARVEST_UPLOAD  # default
-
-            # some GSLA files are gzipped, so gunzip them before checking them
-            # if uploaded file is GZIP check that GZIP contains a NetCDF
-            netcdf_collection = self.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)
-            if len(netcdf_collection) != 1:
-                raise InvalidInputFileError(
-                    "Expecting one netCDF file in GZIP archive '{gzip}'".format(gzip=os.path.basename(self.input_file))
-                )
-
-        netcdf_file_gz = self.file_collection.filter_by_attribute_id('file_type', FileType.GZIP)[0]
-        netcdf_file = self.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)[0]
-        # setting the path of the gz file with the gunzipped file
-        netcdf_file_gz.dest_path = self.dest_path(netcdf_file.src_path)
-        # Nothing to do with *.nc. Talend can harvest *.nc.gz. Set to NO_ACTION
-        netcdf_file.publish_type = PipelineFilePublishType.NO_ACTION
+        # RT and DM NetCDF files to be harvested
+        if GSLA_REGEX.match(netcdf_file.name):
+            netcdf_file.publish_type = PipelineFilePublishType.HARVEST_UPLOAD
 
         # Yearly file should never be harvested, upload only
         if GSLA_REGEX_YEARLY.match(netcdf_file.name):
-            netcdf_file_gz.publish_type = PipelineFilePublishType.UPLOAD_ONLY
+            netcdf_file.publish_type = PipelineFilePublishType.UPLOAD_ONLY
 
     @staticmethod
     def dest_path(filepath):
-        " Netcdf only as an input. Not nc.gz"
+        " Netcdf only as an input"
         file_basename = os.path.basename(filepath)
         gsla_type = get_gsla_type(filepath)
         # the facility does not want the version number in the directory name anymore
@@ -110,6 +85,4 @@ class GslaHandler(HandlerBase):
             gsla_year = datetime.strptime(fields['nc_time_cov_start'], '%Y%m%dT%H%M%SZ').year
             dest_path_val = os.path.join(GSLA_PREFIX_PATH, gsla_type, str(gsla_year), file_basename)
 
-        # destination path should always be for nc.gz files. So we force it
-        return '{val}.gz'.format(val=dest_path_val) if dest_path_val.endswith('.nc') else dest_path_val
-
+        return '{val}'.format(val=dest_path_val)
--- test_aodndata/gsla/test_gsla.py
+++ test_aodndata/gsla/test_gsla.py
@@ -1,4 +1,3 @@
-import gzip
 import os
 import shutil
 import unittest
@@ -16,12 +15,12 @@ from aodndata.gsla.handler import GslaHandler, GSLA_PREFIX_PATH, GSLA_REGEX
 
 TEST_ROOT = os.path.join(os.path.dirname(__file__))
 
-GOOD_NC_GZ_NRT = os.path.join(TEST_ROOT, "IMOS_OceanCurrent_HV_20220101T000000Z_GSLA_FV02_NRT.nc.gz")
-GOOD_NC_GZ_DM02 = os.path.join(TEST_ROOT, "IMOS_OceanCurrent_HV_20000101T000000Z_GSLA_FV02_DM02.nc.gz")
+GOOD_NC_NRT = os.path.join(TEST_ROOT, "IMOS_OceanCurrent_HV_20220101T000000Z_GSLA_FV02_NRT.nc")
+GOOD_NC_DM02 = os.path.join(TEST_ROOT, "IMOS_OceanCurrent_HV_20000101T000000Z_GSLA_FV02_DM02.nc")
 
-GOOD_YEARLY_FILE_DM02 = os.path.join(TEST_ROOT, 'IMOS_OceanCurrent_HV_2018.nc.gz')
+GOOD_YEARLY_FILE_DM02 = os.path.join(TEST_ROOT, 'IMOS_OceanCurrent_HV_2018.nc')
 
-BAD_PATH = os.path.join(TEST_ROOT, 'bad.nc.gz')
+BAD_PATH = os.path.join(TEST_ROOT, 'bad.nc')
 
 
 class TestGslaHandler(HandlerTestCase):
@@ -29,48 +28,19 @@ class TestGslaHandler(HandlerTestCase):
         self.handler_class = GslaHandler
         super(TestGslaHandler, self).setUp()
 
-    def test_push_unzipped_nc(self):
-        """ check on pushing *.nc rather than *.nc.gz """
-
-        with TemporaryDirectory() as tmpdir:
-            with gzip.open(GOOD_NC_GZ_DM02, 'rb') as f_in:
-                good_nc_dm02 = os.path.join(tmpdir, os.path.basename(GOOD_NC_GZ_DM02.replace('.gz', '')))
-                with open(good_nc_dm02, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-
-            handler = self.run_handler(good_nc_dm02,
-                                   check_params={'checks': ['cf:1.6', 'imos:1.4']}
-                                   )
-            self.assertEqual(len(handler.file_collection), 2)
-            f_nc = handler.file_collection.filter_by_attribute_value('file_type', FileType.NETCDF)[0]
-            f_gz = handler.file_collection.filter_by_attribute_value('file_type', FileType.GZIP)[0]
-
-            self.assertEqual(f_nc.check_type, PipelineFileCheckType.NC_COMPLIANCE_CHECK)
-            self.assertEqual(f_nc.publish_type, PipelineFilePublishType.NO_ACTION)
-            self.assertEqual(f_gz.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
-
-            expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/2000", os.path.basename(GOOD_NC_GZ_DM02))
-            self.assertEqual(expected_path, f_gz.dest_path)
-
-
     def test_good_nc(self):
         """ check various good dest_path """
-        dest_path = GslaHandler.dest_path(GOOD_NC_GZ_NRT)
-        expected_path = os.path.join(GSLA_PREFIX_PATH, "NRT/2022", os.path.basename(GOOD_NC_GZ_NRT))
+        dest_path = GslaHandler.dest_path(GOOD_NC_NRT)
+        expected_path = os.path.join(GSLA_PREFIX_PATH, "NRT/2022", os.path.basename(GOOD_NC_NRT))
         self.assertEqual(expected_path, dest_path)
 
-        dest_path = GslaHandler.dest_path(GOOD_NC_GZ_DM02)
-        expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/2000", os.path.basename(GOOD_NC_GZ_DM02))
+        dest_path = GslaHandler.dest_path(GOOD_NC_DM02)
+        expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/2000", os.path.basename(GOOD_NC_DM02))
         self.assertEqual(expected_path, dest_path)
 
-        # for YEARLY_FILES, the dest_path will need to read the global attributes to find the product_type. In this case
-        # since we're only testing the dest_path function and not the hanlder. we're ungzipping the gz
-        nc_file_gunzip_path = os.path.join(self.temp_dir, os.path.basename(GOOD_YEARLY_FILE_DM02.rstrip('.gz')))
-        with open(os.path.join(self.temp_dir, os.path.basename(GOOD_YEARLY_FILE_DM02.rstrip('.gz'))), "wb") as tmp:
-            shutil.copyfileobj(gzip.open(GOOD_YEARLY_FILE_DM02), tmp)
-            dest_path = GslaHandler.dest_path(nc_file_gunzip_path)
-            expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/yearfiles", os.path.basename(GOOD_YEARLY_FILE_DM02))
-            self.assertEqual(expected_path, dest_path)
+        dest_path = GslaHandler.dest_path(GOOD_YEARLY_FILE_DM02)
+        expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/yearfiles", os.path.basename(GOOD_YEARLY_FILE_DM02))
+        self.assertEqual(expected_path, dest_path)
 
     def test_bad_file(self):
         self.run_handler_with_exception(InvalidFileNameError, BAD_PATH)
@@ -79,44 +49,38 @@ class TestGslaHandler(HandlerTestCase):
     def test_bad_prefix(self):
         """ test case to add allowed_dest_path_regexes to json"""
         self.run_handler_with_exception(AttributeValidationError,
-                                        GOOD_NC_GZ_DM02,
+                                        GOOD_NC_DM02,
                                         allowed_dest_path_regexes=["IMOS/OceanCurrent/GSLA"])
 
     def test_get_fields(self):
         """ test basic function outputs"""
-        fields = get_pattern_subgroups_from_string(os.path.basename(GOOD_NC_GZ_DM02), GSLA_REGEX)
+        fields = get_pattern_subgroups_from_string(os.path.basename(GOOD_NC_DM02), GSLA_REGEX)
         self.assertEqual(fields['nc_time_cov_start'], '20000101T000000Z')
         self.assertEqual(fields['product_type'], 'DM02')
 
     def test_good_dm02(self):
-        handler = self.run_handler(GOOD_NC_GZ_DM02,
+        handler = self.run_handler(GOOD_NC_DM02,
                                    check_params={'checks': ['cf:1.6', 'imos:1.4']}
                                    )
-        self.assertEqual(len(handler.file_collection), 2)
+        self.assertEqual(len(handler.file_collection), 1)
         f_nc = handler.file_collection.filter_by_attribute_value('file_type', FileType.NETCDF)[0]
-        f_gz = handler.file_collection.filter_by_attribute_value('file_type', FileType.GZIP)[0]
 
         self.assertEqual(f_nc.check_type, PipelineFileCheckType.NC_COMPLIANCE_CHECK)
-        self.assertEqual(f_nc.publish_type, PipelineFilePublishType.NO_ACTION)
-        self.assertEqual(f_gz.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
-        self.assertEqual(f_gz.name, os.path.basename(GOOD_NC_GZ_DM02))
+        self.assertEqual(f_nc.publish_type, PipelineFilePublishType.HARVEST_UPLOAD)
+        self.assertEqual(f_nc.name, os.path.basename(GOOD_NC_DM02))
 
-        expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/2000", os.path.basename(GOOD_NC_GZ_DM02))
-        self.assertEqual(expected_path, f_gz.dest_path)
+        expected_path = os.path.join(GSLA_PREFIX_PATH, "DM/2000", os.path.basename(GOOD_NC_DM02))
+        self.assertEqual(expected_path, f_nc.dest_path)
 
     def test_push_yearly_file(self):
         """
-        Test case: Push new yearly file *.nc.gz with UPLOAD_ONLY
+        Test case: Push new yearly file *.nc with UPLOAD_ONLY
         """
         # run the handler
         handler = self.run_handler(GOOD_YEARLY_FILE_DM02)
 
-        nc_gz_file = handler.file_collection.filter_by_attribute_id('file_type', FileType.GZIP)[0]
-        self.assertEqual(nc_gz_file.publish_type, PipelineFilePublishType.UPLOAD_ONLY)
-
         nc_file = handler.file_collection.filter_by_attribute_id('file_type', FileType.NETCDF)[0]
-        self.assertEqual(nc_file.publish_type, PipelineFilePublishType.NO_ACTION)
-
+        self.assertEqual(nc_file.publish_type, PipelineFilePublishType.UPLOAD_ONLY)
 
 if __name__ == '__main__':
     unittest.main()
