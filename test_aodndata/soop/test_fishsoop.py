557ccfbc811d9a85c21598f17dfe6a9326ee7275
lbesnard <laurent.besnard@utas.edu.au>
2024-05-28 17:24:01 +1000

    (Fix) FishSOOP needs to be UPLOAD_ONLY + fix cython

--- test_aodndata/soop/test_fishsoop.py
+++ test_aodndata/soop/test_fishsoop.py
@@ -1,26 +1,34 @@
 import os
 import unittest
 
-from aodncore.pipeline.exceptions import InvalidFileNameError
-from aodndata.soop.soop_fishsoop import dest_path_soop_fishsoop
+from aodndata.soop.soop_fishsoop import SoopFishSoopHandler
+from aodncore.pipeline import PipelineFileCheckType, PipelineFilePublishType
+from aodncore.testlib import HandlerTestCase
 
 TEST_ROOT = os.path.join(os.path.dirname(__file__))
+NC_FILE_NRT = 'IMOS_SOOP-FishSOOP_TP_20240310T094642Z_FV00_786.nc'
 
 
-class TestSoopFishSoop(unittest.TestCase):
+class TestSoopFishSoop(HandlerTestCase):
+    def setUp(self):
+        self.handler_class = SoopFishSoopHandler
+        super(TestSoopFishSoop, self).setUp()
+        self.nc_file_nrt = os.path.join(TEST_ROOT, NC_FILE_NRT)
 
     def test_dest_path_soop_fishsoop(self):
-        good_nc_nrt = os.path.join(TEST_ROOT, 'IMOS_SOOP-FishSOOP_PT_20240310T094642Z_FV00_786.nc')
-        self.assertEqual(dest_path_soop_fishsoop(good_nc_nrt),
-                         'IMOS/SOOP/SOOP-FishSOOP/REALTIME/2024/03/{basename}'.format(
-                             basename=os.path.basename(good_nc_nrt)),
-        )
+        handler = self.run_handler(self.nc_file_nrt,
+                                   check_params={'checks': ['cf:1.6', 'imos:1.4']}
+                                   )
 
-        good_nc_dm = os.path.join(TEST_ROOT, 'IMOS_SOOP-FishSOOP_TP_20240310T094642Z_FV01_786.nc')
-        self.assertEqual(dest_path_soop_fishsoop(good_nc_dm),
-                         'IMOS/SOOP/SOOP-FishSOOP/DELAYED/2024/03/{basename}'.format(
-                             basename=os.path.basename(good_nc_dm)),
-        )
+        f = handler.file_collection[0]
+
+        self.assertEqual(f.check_type, PipelineFileCheckType.NC_COMPLIANCE_CHECK)
+        self.assertEqual(f.publish_type, PipelineFilePublishType.UPLOAD_ONLY)
+        self.assertEqual(f.dest_path,
+                         os.path.join(f"IMOS/SOOP/SOOP-FishSOOP/REALTIME/2024/03/{os.path.basename(self.nc_file_nrt)}"))
+
+        self.assertTrue(f.is_checked)
+        self.assertTrue(f.is_stored)
 
 
 if __name__ == '__main__':
